7a89daf9184accd8f44676e98ed78ba75e428e18
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-10 22:25:47 -0300

    js.22.02.10.01

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log('Hello world !!!')
 console.log('Curso de JavaScript ...')
 console.log('Eu estou programando em js !!!')
 console.log('O professor é o Luiz Otávio Miranda.')
+console.log('Eu estou lendo de mais os e-mails')
+console
