b4a20974bc2a159adea8a64d7009015304438770
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-20 14:51:54 -0300

    js.22.02.20.02

--- index.js
+++ index.js
@@ -19,3 +19,4 @@ console.log(
 console.log('Anderson de Oliveira Santos ou Son !!!')
 console.log('Mais um dia chega ao final !!!')
 // Fazeu um anotação para lembrete !!!
+// Tudo que está como comentário, será ignorado e não aparecerá na página ...
