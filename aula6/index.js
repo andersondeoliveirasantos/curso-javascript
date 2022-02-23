192ffacb5caf810dd94fde581140db524377c708
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-23 00:17:00 -0300

    variaveis.02

--- aula6/index.js
+++ aula6/index.js
@@ -1,5 +1,7 @@
-console.log('João nasceu em 1984.')
-console.log('Em 2000 João conheceu Maria.')
-console.log('João casou-se com Maria em 2012.')
-console.log('Maria teve 01 filho com João em 2015.')
-console.log('O filho de João se chama Eduardo.')
+let nome = 'Aldo' // String
+
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 2000', nome, 'conheceu Maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
+console.log('Maria teve 01 filho com', nome, 'em 2015.')
+console.log('O filho de', nome, 'se chama Eduardo.')
