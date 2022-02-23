4ed30babd9774e3163f7fe156f8d6bd7b3e763e6
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-22 21:45:31 -0300

    js.22.02.22.04

---
+++ aula3/index.js
@@ -0,0 +1,29 @@
+console.log('Hello world !!!')
+console.log('Curso de JavaScript ...')
+console.log('Eu estou programando em js !!!')
+console.log('O professor é o Luiz Otávio Miranda.')
+console.log('Estou adorando o novo curso.')
+console.log('Mais um dia de estudo.')
+console.log('Mais um avanço.')
+console.log('Mais um aprendizado.')
+console.log('É fácil de aprender')
+console.log('Luiz Otávio')
+console.log('Luiz Otávio')
+console.log(`Luiz Otávio`)
+console.log(123456789)
+console.log(
+  'Meu nome é Anderson, e estou aprendendo JavaScript as',
+  22,
+  'horas.'
+)
+console.log('Anderson de Oliveira Santos ou Son !!!')
+console.log('Mais um dia chega ao final !!!')
+// Fazeu um anotação para lembrete !!!
+// Tudo que está como comentário, será ignorado e não aparecerá na página ...
+console.log('Hello World')
+console.log('Linha 01')
+console.log('Linha 02')
+console.log('Linha 03')
+console.log('Linha 04')
+console.log('6. Navegador vs Node (HTML + JavaScript)')
+console.log('Dor no ombro')
