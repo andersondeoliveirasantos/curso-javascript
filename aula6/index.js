a9b2bdf7e90b1c2a07eca5990e084b14ece203c7
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-25 23:27:49 -0300

    comentarios.03

--- aula6/index.js
+++ aula6/index.js
@@ -1,7 +1,7 @@
 let nome = 'Aldo' // String
 
 console.log(nome, 'nasceu em 1984.')
-console.log('Em 2000', nome, 'conheceu Maria.')
+// console.log('Em 2000', nome, 'conheceu Maria.')
 console.log(nome, 'casou-se com Maria em 2012.')
 /*
 console.log('Maria teve 01 filho com', nome, 'em 2015.')
