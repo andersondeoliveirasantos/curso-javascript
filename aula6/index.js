a1280aa95d58c2c58d2a91d2e748e6f840ed2bb3
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-25 23:28:35 -0300

    comentarios.04

--- aula6/index.js
+++ aula6/index.js
@@ -1,5 +1,5 @@
 let nome = 'Aldo' // String
-
+// Podemos fazer vários comentários !!!
 console.log(nome, 'nasceu em 1984.')
 // console.log('Em 2000', nome, 'conheceu Maria.')
 console.log(nome, 'casou-se com Maria em 2012.')
