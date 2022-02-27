c5ceffa6cfaf0117b657db81b6d6a56be629360a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-27 14:36:17 -0300

    Const e Let

---
+++ aula8/index.js
@@ -0,0 +1,9 @@
+/* 
+Anderson de Oliveira Santos tem 40 anos, pesa 92 kg, tem 1.86 de altura e seu IMC é de 25.925925925925925924
+*/
+const nome = 'Anderson'
+const sobrenome = ' de Oliveira Santos'
+const idade = 40
+const peso = 92
+const altura = 1.86
+let imc // PESO / (ALTURA * ALTURA)
