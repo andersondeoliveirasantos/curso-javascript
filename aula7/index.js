c70309ae47ce1699f764a34a6a7fee77cfbea569
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-27 13:25:36 -0300

    constantesComConst

---
+++ aula7/index.js
@@ -0,0 +1,7 @@
+// Regras básicas das variáveis !!!
+// Não podemos criar variáveis com palavras reservadas.
+// Variáveis precisam ter nomes significativos.
+// Não pode começas o nome de uma variável com número.
+// Utilizamos camelCase ( letras maiúsculas e minúscula, sem espaços).
+// Não podemos redeclarar variáveis com let
+// Não utilize VAR, utilize LET !!!
