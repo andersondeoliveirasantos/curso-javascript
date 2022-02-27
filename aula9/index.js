c6b358c70c0daff7eb71bd5c1b65a2fd6ea8140e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-27 15:55:31 -0300

    letVsVar.03

--- aula9/index.js
+++ aula9/index.js
@@ -1,3 +1,5 @@
+// String, number, undefined, null, boolean
+
 var nome = 'Anderson'
 var nome = 'de Oliveira Santos'
 
@@ -5,3 +7,17 @@ console.log(nome)
 
 nome = 'Anderson' // NÃO FAÇA ISSO
 // Dessa forma, você está criando uma variável GLOBAL, isso pode impactar o seu códico de diversas formas.
+
+const nome = 'Anderson' // string
+const nome = 'Anderson' // string
+const nome = `Anderson` // string
+
+const num1 = 10 // number
+const num2 = 10.52 // number
+
+let nomeAluno // underfined = não aponta para local nenhum na memória
+let sobrenomeAluno = null // Nulo -> não aponta para local nenhum na memória
+
+const boolean = false // FALSO ou VERDADEIRO
+
+console.log(typeof nome, nome) // verificar o tipo e/ou do conteúdo da linha
