65b7859cfc144b10d18a680cf035c3b3406d115e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-27 13:30:05 -0300

    variaveisComLet.03

--- aula7/index.js
+++ aula7/index.js
@@ -1,7 +1,11 @@
 // Regras básicas das variáveis !!!
-// Não podemos criar variáveis com palavras reservadas.
-// Variáveis precisam ter nomes significativos.
-// Não pode começas o nome de uma variável com número.
+// Não podemos criar constantes com palavras reservadas.
+// Constantes precisam ter nomes significativos.
+// Não pode começas o nome de uma constante com número.
 // Utilizamos camelCase ( letras maiúsculas e minúscula, sem espaços).
-// Não podemos redeclarar variáveis com let
-// Não utilize VAR, utilize LET !!!
+// Case-sensitive.
+// Não pode modificar o valor de uma constante.
+// Não utilize VAR, utilize CONST !!!
+
+const nome = 'Anderson'
+console.log(nome)
