3306dc9fb547bdf8572266848596a38c5e6b79bc
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-02-27 15:37:43 -0300

    letVsVar.02

---
+++ aula9/index.js
@@ -0,0 +1,7 @@
+var nome = 'Anderson'
+var nome = 'de Oliveira Santos'
+
+console.log(nome)
+
+nome = 'Anderson' // NÃO FAÇA ISSO
+// Dessa forma, você está criando uma variável GLOBAL, isso pode impactar o seu códico de diversas formas.
