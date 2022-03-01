9ca9c2d155a0be4455a12b90082c676437615041
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-01 15:14:07 -0300

    operadoresAritmeticos.03

--- aula10/index.js
+++ aula10/index.js
@@ -1,23 +1,28 @@
 /*
 
-*Aritimeticos
+*OPERADORES ATITIMÉTICOS !!!
 
 * + ( Adição / Concatenação )
-
 - ( Subtração )
-
 / ( Divisão )
-
 * ( Multiplicação )  
-
 ** (Potenciação)
-
 % (Resto da divisão)
+*/
+/*
+
+ORDEM DE PRECEDÊNCIA !!!
+() > Parenteses
+** > Potenciação
+* / % > Multiplicação > Divisão > Módulo da divisão
++ - > Adição > Subtração
+
 
 */
 
 const num1 = 10
 const num2 = 5
+const num3 = 2
 
 console.log(num1 + num2)
 console.log(num1 - num2)
@@ -25,3 +30,12 @@ console.log(num1 * num2)
 console.log(num1 / num2)
 console.log(num1 ** num2)
 console.log(num1 % num2)
+console.log((num1 + num2) * num3)
+console.log((num1 * num2) / num3)
+
+let contador = 1
+contador++ // 2
+contador++ // 3
+++contador // 4
+++contador // 5
+console.log(contador)
