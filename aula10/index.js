d21f761a7208216a6e45347e99a78d7cc7202fdb
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-01 14:57:30 -0300

    operadoresAritmeticos.02

---
+++ aula10/index.js
@@ -0,0 +1,27 @@
+/*
+
+*Aritimeticos
+
+* + ( Adição / Concatenação )
+
+- ( Subtração )
+
+/ ( Divisão )
+
+* ( Multiplicação )  
+
+** (Potenciação)
+
+% (Resto da divisão)
+
+*/
+
+const num1 = 10
+const num2 = 5
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 ** num2)
+console.log(num1 % num2)
