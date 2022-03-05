1922bbd5211b18945dd46194565c43ac750b2a6f
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-05 17:13:39 -0300

    confirm.01

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,3 +1,6 @@
 alert('Tudo tranquilo ??? ')
 alert('Sou um programador !!!')
 window.alert('Mesma função, que a linha acima')
+
+confirm('Função, confirmar ...')
+window.confirm('Deseja realmente apagar ???')
