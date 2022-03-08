17704b2252ccf2badb546a7ffe8dabdc989350c9
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-07 22:54:11 -0300

    string.02

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -5,3 +5,8 @@ console.log(umaString)
 console.log(umaString[4])
 console.log(umaString[13])
 console.log(umaString[-3])
+
+console.log(`${umaString} em um lindo dia.`)
+
+console.log(umaString.indexOf('o'))
+console.log(umaString.lastIndexOf('x'))
