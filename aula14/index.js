e7a9ed6296f81c0dce6c017c295065f1c9f0fb51
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-13 15:58:09 -0300

    numbers.03

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,7 @@
-let num1 = 1
+let num1 = 10
 let num2 = 2.5
 
-console.log(num1.toString() + num2)
-console.log(typeof num1)
+// console.log(num1.toString() + num2)
+// num1 = num1.toString()
+// console.log(num1.toString(2))
+// console.log(num1.toFixed(2))
