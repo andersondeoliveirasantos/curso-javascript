896aab7e51ad3419c93d80f48907e0ce1d616142
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-14 23:14:27 -0300

    numbers.07

--- aula14/index.js
+++ aula14/index.js
@@ -6,16 +6,16 @@ let num2 = 0.1 // number
 num1 += num2 // 0.8
 num1 += num2 // 0.9
 num1 += num2 // 1.0
-num1 += num2 // 1.1
-num1 += num2 // 1.2
-num1 += num2 // 1.3
-num1 += num2 // 1.4
-num1 += num2 // 1.5
-num1 += num2 // 1.6
-num1 += num2 // 1.7
-num1 += num2 // 1.8
-num1 += num2 // 1.9
-num1 += num2 // 2.0
+// num1 += num2 // 1.1
+// num1 += num2 // 1.2
+// num1 += num2 // 1.3
+// num1 += num2 // 1.4
+// num1 += num2 // 1.5
+// num1 += num2 // 1.6
+// num1 += num2 // 1.7
+// num1 += num2 // 1.8
+// num1 += num2 // 1.9
+// num1 += num2 // 2.0
 
 num1 = parseFloat(num1.toFixed(2))
 
