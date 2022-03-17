8dd87bd27a315ed771aaa59df9431ee2399ec4cd
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-16 23:43:04 -0300

    numbers.08

---
+++ aula14/tests.js
@@ -0,0 +1,8 @@
+// console.log(num1.toString() + num2)
+// num1 = num1.toString()
+// console.log(num1.toString(2))
+// console.log(num1.toFixed(2))
+// console.log(Number.isInteger(num1))
+// console.log(Number.isNaN(temp))
+// let temp = num1 + '5'
+// console.log(temp)
