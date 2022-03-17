7e14131a82349773d60bc12ea68e95ca6fc7f58e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-17 01:35:47 -0300

    numbers.09

--- aula14/tests.js
+++ aula14/tests.js
@@ -1,3 +1,14 @@
+// num1 += num2 // 1.1
+// num1 += num2 // 1.2
+// num1 += num2 // 1.3
+// num1 += num2 // 1.4
+// num1 += num2 // 1.5
+// num1 += num2 // 1.6
+// num1 += num2 // 1.7
+// num1 += num2 // 1.8
+// num1 += num2 // 1.9
+// num1 += num2 // 2.0
+
 // console.log(num1.toString() + num2)
 // num1 = num1.toString()
 // console.log(num1.toString(2))
