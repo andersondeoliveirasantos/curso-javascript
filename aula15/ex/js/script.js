ec7517e5c2622dcd2db438bf69896c8651449a9a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-19 07:51:22 -0300

    exNumbersMath.02

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -1,2 +1,20 @@
-const numero = prompt('Digite um número')
-document
+const numero = Number(prompt('Digite um número:'))
+
+const numeroTitulo = document.getElementById('numero-titulo')
+const texto = document.getElementById('texto')
+
+numeroTitulo.innerHTML = numero
+
+texto.innerHTML = `<p>Seu número + 2 é ${numero + 2}.</p>`
+
+texto.innerHTML = `<p>Raíz quadrada: ${numero ** 0.5}</p>`
+
+texto.innerHTML = `<p>${numero} é inteiro: ${Number.isInteger(numero)}.</p>`
+
+texto.innerHTML = `<p>É NaN: ${Number.isNaN(numero)}.</p>`
+
+texto.innerHTML = `<p>Arredondadno para baixo: ${Math.floor(numero)}.</p>`
+
+texto.innerHTML = `<p>Arredondando para cima: ${Math.ceil(numero)}.</p>`
+
+texto.innerHTML = `<p>Com duas casas decimais: ${numero.toFixed(2)}.</p>`
