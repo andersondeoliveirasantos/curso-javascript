d22c16a993355bc18077c4b0eff8c4d581716582
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-19 08:14:57 -0300

    arrays.22.03.19.02

--- aula16/index.js
+++ aula16/index.js
@@ -1,1 +1,7 @@
+//            123456789...
+const nome = 'Anderson de Oliveira Santos'
+
+// Um ìndice para cada nome
+//              0       1        2
 const alunos = ['Luiz', 'Maria', 'João']
+console.log(alunos)
