5ac20c0722fbbed61dcfefaf429d950a23d6b230
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-18 22:40:27 -0300

    objetoMath.02

--- aula15/index.js
+++ aula15/index.js
@@ -1,6 +1,8 @@
 let num1 = 9.54578
 // let num2 = Math.floor(num1) // Aredonda para cima
 // let num2 = Math.ceil(num1) // Aredonda para baixo
-let num2 = Math.round(num1)
+// let num2 = Math.round(num1) // Aredonda para o mais próximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6))
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6))
 
 console.log(num2)
