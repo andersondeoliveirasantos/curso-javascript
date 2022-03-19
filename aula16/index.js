0d3f1d352d4ae63ca0f5fa7f83cf0efc0149711d
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-19 18:58:45 -0300

    arrays.22.03.19.05

--- aula16/index.js
+++ aula16/index.js
@@ -6,7 +6,22 @@
 //              0       1        2
 const alunos = ['Luiz', 'Maria', 'João']
 
-console.log(alunos)
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+// console.log(alunos.slice(0, -2)) Fatiamento do indice
+
+// console.log(alunos[50])
+
+// delete alunos[1] Deleta o indice e deixa o espaço vazio ( undefined )
+// console.log(alunos[1])
+
+// const removido = alunos.shift() // Remove o ultimo indice
+// const removido = alunos.pop() // Remove o primeiro indice, const salva o indice removido
+// console.log(removido)
+
+/// alunos.unshift('Luiza') // Adiciona no começo
+// alunos.unshift('Fábio')
 
 // alunos.push('Luiza') // Adiciona no fim
 // alunos.push('Fábio')
