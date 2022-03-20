149d5bc060f8394195facd58905abe0cd238818b
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-20 08:13:01 -0300

    valoresPrimitivosReferencia.22.03.20.02

--- aula19/index.js
+++ aula19/index.js
@@ -1,12 +1,33 @@
 /*
 TIPO DE DADOS 
 
-Primitivos (imutáveis) - String, number, boolean, undefined, null, bigint, 
+Primitivos (imutáveis) - String, number, boolean, undefined, null, bigint - VALORES COPIADOS
 
-Referência (mutável) - Arrays, object, function
-
+Referência (mutável) - Arrays, object, function - VALORES PASSADS POR REFERÊNCIA
 */
 
+/*
 let a = [1, 2, 3]
-let b = a
+let b = [...a]
+let c = b
 console.log(a, b)
+
+a.push(4)
+console.log(a, b)
+
+b.pop()
+console.log(a, b)
+
+a.push('Anderson')
+console.log(a, c)
+*/
+
+const a = {
+  nome: 'Anderson',
+  sobrenome: 'de Oliveira Santos'
+}
+const b = { ...a }
+
+a.nome = 'João'
+console.log(a)
+console.log(b)
