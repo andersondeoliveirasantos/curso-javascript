19d8af5e60666a293078f52d6e18c860ece58444
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-19 23:02:23 -0300

    objetos.22.03.19.06

---
+++ aula18/index.js
@@ -0,0 +1,44 @@
+/*
+let array = [1, 2, 3]
+array.push(4)
+array[0] = 'Anderson'
+array = 'Outra'
+console.log(array)
+*/
+
+/*
+const nome01 = 'Anderson'
+const sobrenome01 = 'de Oliveira Santos'
+const idade01 = 40
+
+const nome02 = 'Daniela Cristina'
+const sobrenome02 = 'da Silva de Oliveira'
+const idade02 = 35
+
+console.log(
+  `O primeiro condidato é o ${nome01} ${sobrenome01}, que tem a idade de ${idade01} anos.`
+)
+console.log(
+  `A segunda candidata é a ${nome02} ${sobrenome02}, que tem a idade de ${idade02} anos.`
+)
+*/
+
+const pessoa1 = {
+  nome: 'Anderson',
+  sobrenome: 'de Oliveira SAntos',
+  idade: 40
+}
+
+console.log(pessoa1.nome)
+console.log(pessoa1.sobrenome)
+console.log(pessoa1.idade)
+
+const pessoa2 = {
+  nome: 'Daniela Cristina',
+  sobrenome: 'da Silve de Oliveira',
+  idade: 35
+}
+
+console.log(pessoa2.nome)
+console.log(pessoa2.sobrenome)
+console.log(pessoa2.idade)
