21811bd903769046cde0b383231e9202a747b598
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-20 07:43:53 -0300

    valoresPrimitivosReferencia.22.03.20.01

---
+++ aula19/index.js
@@ -0,0 +1,12 @@
+/*
+TIPO DE DADOS 
+
+Primitivos (imutáveis) - String, number, boolean, undefined, null, bigint, 
+
+Referência (mutável) - Arrays, object, function
+
+*/
+
+let a = [1, 2, 3]
+let b = a
+console.log(a, b)
