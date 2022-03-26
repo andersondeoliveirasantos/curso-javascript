db63817dd048644708759e6ebcf23326513505ec
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-26 20:39:26 -0300

    ExFuncaoArry.05

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -1,18 +1,15 @@
 function meuEscopo() {
   const form = document.querySelector('.form')
 
-  /*
-  form.onsubmit = function (evento) {
-    evento.preventDefault()
-    alert('Realemte deseja emviar?')
-    console.log('Os dados foram enviado')
-  }
-  */
-  let contador = 1
   function recebeEventoForm(evento) {
     evento.preventDefault()
-    console.log(`Form não foi enviado ${contador}`)
-    contador++
+
+    const nome = form.querySelector('.nome')
+    const sobrenome = form.querySelector('.sobrenome')
+    const peso = form.querySelector('.peso')
+    const altura = form.querySelector('.altura')
+
+    console.log(nome, sobrenome, peso, altura)
   }
 
   form.addEventListener('Submit', receberEventoForm)
