fe3bf34164a62d2cfdf3d267492c5ba13e2bab30
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-27 06:49:28 -0300

    OperadoresLogicos002

--- aula22/index.js
+++ aula22/index.js
@@ -2,9 +2,11 @@
 
 OPERADORES LÓGICOS
 
-&& -> AND -> E
+&& -> AND -> E ( Todas as expressões precisam ser verdadeiras para retornar true )
 || -> OR  -> OU
 !  -> NOT -> NÃO
 
-
 */
+
+const expressao = true && true && false && true
+console.log(expressao)
