1bda03186da5bccdbe0abce657ec731d636ae948
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-27 07:05:12 -0300

    OperadoresLogicos003

--- aula22/index.js
+++ aula22/index.js
@@ -3,10 +3,24 @@
 OPERADORES LÓGICOS
 
 && -> AND -> E ( Todas as expressões precisam ser verdadeiras para retornar true )
-|| -> OR  -> OU
-!  -> NOT -> NÃO
+|| -> OR  -> OU ( Apenas uma expressão precisa ser verdadeira para retornar true )
+!  -> NOT -> NÃO (Nega a expressão, se for true vai aparecer false, se for false, vai aparecer true )
 
 */
 
-const expressao = true && true && false && true
-console.log(expressao)
+const expressaoAnd = true && true && false && true
+const expressaoOr = false || false || false || true || false || false
+
+console.log(expressaoAnd)
+
+console.log(expressaoOr)
+
+const usuario = 'Anderson'
+const senha = '123456'
+
+//                       true                    false
+const vaiLogar = usuario === 'Anderson' && senha === '12345'
+console.log(vaiLogar)
+
+//          Nega a expressão
+console.log(!false)
