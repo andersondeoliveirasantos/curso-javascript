86dc33bbbd1f51997394075192bdafcb084aa504
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-26 22:23:09 -0300

    operadoresDeComparacao.002

--- aula21/index.js
+++ aula21/index.js
@@ -6,9 +6,13 @@ OPERADORES DE COMPARAÇÃO !!!
 >=  maior que ou igual a 
 <   menor que 
 <=  menor que ou igual a  
-==  igualdade ( valor )
+==  igualdade ( valor ) *************
 === igualdade estrita ( valor e tipo )
-!=  diferente ( valor )
+!=  diferente ( valor ) *************
 !== diferente estrito ( valor e tipo )
 
 */
+const num1 = 10
+const num2 = 10
+const comp = num1 == num2
+console.log(comp)
