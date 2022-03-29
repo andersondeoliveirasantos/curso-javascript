ed8cb82cbe3e95b7ca051181b6ace2a443c2e5c0
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-03-28 23:28:43 -0300

    excercicio.26.demo.004

--- aula26-demo/js/main.js
+++ aula26-demo/js/main.js
@@ -1,2 +1,7 @@
 // Capturar o evento de submit do formulário
 const form = document.querySelector('#form')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  console.log('Evento previnido')
+})
