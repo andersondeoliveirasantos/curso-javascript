119ca4fed97f4161365d96d88ac426581ffd1e2d
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-02 20:03:08 -0300

    ObjetoDate.002

--- aula28/index.js
+++ aula28/index.js
@@ -2,4 +2,6 @@
 // const tresHoras = 60 * 60 * 3 8 1000 // Equivale a três horas
 // const umDia = 60 * 60 * 24 * 1000
 // const data = new Date(0 + umDia) // 01/01/1970 Timestamp unix ou época unix
-// console.log(data.toISOString())
+
+const data = new Date()
+console.log(data.toISOString())
