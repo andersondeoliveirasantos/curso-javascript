6084463dd2e7dc539b2393006ae92bd29c4c6f02
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-02 20:01:54 -0300

    ObjetoDate.001

---
+++ aula28/index.js
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
+// const tresHoras = 60 * 60 * 3 8 1000 // Equivale a três horas
+// const umDia = 60 * 60 * 24 * 1000
+// const data = new Date(0 + umDia) // 01/01/1970 Timestamp unix ou época unix
+// console.log(data.toISOString())
