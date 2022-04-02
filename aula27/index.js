e266d2bf6753f669d92738cd9bdfdfc0c3f1b89e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-02 19:41:21 -0300

    operacaoTenaria.002

--- aula27/index.js
+++ aula27/index.js
@@ -1,8 +1,12 @@
-// ? :
-const pontuacaoUsuario = 999
+// ( Condição )? 'Valor para verdadeiro ' : 'Valor para falso '
+const pontuacaoUsuario = 500
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal'
+console.log(nivelUsuario)
 
+/* 
 if (pontuacaoUsuario >= 1000) {
   console.log('Usuário VIP')
 } else {
   console.log('Usuário normal')
 }
+*/
