8c589a20acdbce7cc0e67cbb0c9fd8d80c78ab4c
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-03 19:46:42 -0300

    ObjetoDate.008

--- aula28/index.js
+++ aula28/index.js
@@ -8,14 +8,14 @@
 
 // const data = new Date(2022, 3, 20, 15, 14, 27, 500) // Ano, Mês, Dia, Hora, Minuto, Segundo, Milésimo de Segundo
 
-const data = new Date('2019-04-20 20:20:59')
+const data = new Date('2019-04-20 20:15:59')
 console.log('Dia', data.getDate())
-console.log('Mês', data.getMonth())
+console.log('Mês', data.getMonth() + 1) // Mês começa do zero
 console.log('Ano', data.getFullYear())
 console.log('Hora', data.getHours())
 console.log('Minuto', data.getMinutes())
 console.log('Segundo', data.getSeconds())
 console.log('Milésimo de Segundo', data.getMilliseconds())
-console.log('Dia da semana', data.getDay())
+console.log('Dia da semana', data.getDay()) // 0 - Domingo e 6 - Sábado
 
 console.log(data.toISOString())
