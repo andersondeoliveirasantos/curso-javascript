58e3a29f8d5436ad283ca9fe3507ece8749b7bfc
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-03 20:46:22 -0300

    ObjetoDate.012

--- aula28/index.js
+++ aula28/index.js
@@ -23,8 +23,19 @@ console.log(data.toISOString())
 console.log(Date.now())
 */
 
+function zeroAEsquerda(num) {
+  return num >= 10 ? num : `0${num}`
+}
+
 function formataData(data) {
-  // console.log(data)
+  const dia = data.getDate()
+  const mes = data.getMonth() + 1
+  const ano = data.getFullYear()
+  const hora = data.getHours()
+  const minuto = data.getMinutes()
+  const segundos = data.getSeconds()
+
+  return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundos}`
 }
 
 const data = new Date()
