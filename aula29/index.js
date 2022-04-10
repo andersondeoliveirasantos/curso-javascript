b6377d2b80fe07ce7d24c797148904b2e12ad599
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-10 08:00:46 -0300

    switchCase.002

--- aula29/index.js
+++ aula29/index.js
@@ -3,6 +3,32 @@ let diaSemana = data.getDay()
 diaSemana = 7
 let diaSemanaTexto
 
+switch (diaSemana) {
+  case 0:
+    diaSemana = 'Domingo'
+    break
+  case 1:
+    diaSemana = 'Segunda'
+    break
+  case 2:
+    diaSemana = 'Terça'
+    break
+  case 3:
+    diaSemana = 'Quarta'
+    break
+  case 4:
+    diaSemana = 'Quinta'
+    break
+  case 5:
+    diaSemana = 'Sexta'
+    break
+  case 0:
+    diaSemana = 'Sábado'
+    break
+  default:
+    break
+}
+
 /*
 if (diaSemana === 0) {
   diaSemanaTexto = 'Domingo'
