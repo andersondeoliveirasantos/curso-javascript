cffad8cdc04715784e7d5d22739b57ac0b9e21a3
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-12 23:03:54 -0300

    switchCase.013

--- aula29_switch_case/index.js
+++ aula29_switch_case/index.js
@@ -1,37 +1,33 @@
-function getDiaSemanaTexto (diaSemana ) {
+function getDiaSemanaTexto(diaSemana) {
   let diaSemanaTexto
 
   switch (diaSemana) {
-  case 0
-    diaSemanaTexto = 'Sunday'
-    return diaSemanaTexto
-  case 1
-    diaSemanaTexto = 'Monday'
-    return diaSemanaTexto
-  case 2
-    diaSemanaTexto = 'Tuesday'
-    return diaSemanaTexto
-  case 3
-    diaSemanaTexto = 'Wednesday'
-    return diaSemanaTexto
-  case 4 
-    diaSemanaTexto = 'Thursday'
-    return diaSemanaTexto
-  case 5 
-    diaSemanaTexto = 'Friday'
-    return diaSemanaTexto
-  case 6 
-    diaSemanaTexto = 'Saturday'
-    return diaSemanaTexto
+    case 0:
+      diaSemanaTexto = 'Sunday'
+      return diaSemanaTexto
+    case 1:
+      diaSemanaTexto = 'Monday'
+      return diaSemanaTexto
+    case 2:
+      diaSemanaTexto = 'Tuesday'
+      return diaSemanaTexto
+    case 3:
+      diaSemanaTexto = 'Wednesday'
+      return diaSemanaTexto
+    case 4:
+      diaSemanaTexto = 'Thursday'
+      return diaSemanaTexto
+    case 5:
+      diaSemanaTexto = 'Friday'
+      return diaSemanaTexto
+    case 6:
+      diaSemanaTexto = 'Saturday'
+      return diaSemanaTexto
   }
 }
 
 const data = new Date('1987-04-26 00:00:00')
 let diaSemana = data.getDay()
-
-console.log(diaSemana, diaSemanaTexto)
-
-
 
 /* *****************************************
  ************ SWITCH e CASE ********************
@@ -65,7 +61,6 @@ switch (diaSemana) {
 
 console.log(diaSemana, diaSemanaTexto)
  ********************************************* */
-
 
 /* *****************************************
  ************ IF e ELSE ********************
