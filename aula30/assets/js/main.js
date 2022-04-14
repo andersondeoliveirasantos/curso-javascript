80e7f5ed54111a5fa43fac7339133e71792ececa
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-13 22:48:23 -0300

    switchCase.018

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -1,3 +1,30 @@
-const h1 = document.querySelector('.cotainer h1')
+const h1 = document.querySelector('.container h1')
 const data = new Date()
-h1.innerHTML = data.toString()
+
+function getDayWeekTest(dayWeek) {
+  let dayWeekTest
+
+  switch (dayWeek) {
+    case 0:
+      dayWeekTest = 'Sunday'
+      return dayWeekTest
+    case 1:
+      dayWeekTest = 'Monday'
+      return dayWeekTest
+    case 2:
+      dayWeekTest = 'Tuesday'
+      return dayWeekTest
+    case 3:
+      dayWeekTest = 'Wednesday'
+      return dayWeekTest
+    case 4:
+      dayWeekTest = 'Thursday'
+      return dayWeekTest
+    case 5:
+      dayWeekTest = 'Friday'
+      return dayWeekTest
+    case 6:
+      dayWeekTest = 'Saturday'
+      return dayWeekTest
+  }
+}
