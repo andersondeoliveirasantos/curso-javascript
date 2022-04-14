4f9707dc36d90b8fb292f9277a549caddd2008bd
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-13 22:58:21 -0300

    switchCase.019

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -28,3 +28,33 @@ function getDayWeekTest(dayWeek) {
       return dayWeekTest
   }
 }
+
+function getNameMonth(numberMonth) {
+  let nameMonth
+
+  switch (numberMonth) {
+    case 0:
+      nameMonth = 'January'
+      return nameMonth
+    case 1:
+      dayWeekTest = 'Monday'
+      return dayWeekTest
+    case 2:
+      dayWeekTest = 'Tuesday'
+      return dayWeekTest
+    case 3:
+      dayWeekTest = 'Wednesday'
+      return dayWeekTest
+    case 4:
+      dayWeekTest = 'Thursday'
+      return dayWeekTest
+    case 5:
+      dayWeekTest = 'Friday'
+      return dayWeekTest
+    case 6:
+      dayWeekTest = 'Saturday'
+      return dayWeekTest
+  }
+}
+
+h1.innerHTML = getDayWeekTest(data.getDay())
