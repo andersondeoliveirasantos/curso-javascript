9dff8e021f026ff1efc372169166f7916d5cb4f8
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 08:17:13 -0300

    atribuicaoViaDesstruturacao-Objetos.001

---
+++ aula33-aula32-atribuicaoViaDesstruturacao-Objetos/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+  nome: 'Anderson',
+  sobrenome: 'de Oliveira Santos',
+  idade: 40,
+  endereco: {
+    rua: 'Av. Guilherme Gonçalves do Rosário',
+    numero: 326
+  }
+}
+
+const nome = pessoa.nome
+console.log(pessoa.nome)
