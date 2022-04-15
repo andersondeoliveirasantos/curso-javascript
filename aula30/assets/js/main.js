9aef65eae428e6d017aba80a71ee92a065808e73
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 00:00:59 -0300

    switchCase.025

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -101,7 +101,7 @@ const h1 = document.querySelector('.container h1')
 const data = new Date()
 const opcoes = {
   dateStyle: 'full',
-  timeStyle: 'short'
+  TimeStyle: 'short'
 }
 
 h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes)
