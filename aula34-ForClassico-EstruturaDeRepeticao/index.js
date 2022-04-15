ae63c2d67cfe0bad769d3a732938efa3a21b91a4
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 20:22:31 -0300

    ForClassicoEstruturaDeRepeticao.002

--- aula34-ForClassico-EstruturaDeRepeticao/index.js
+++ aula34-ForClassico-EstruturaDeRepeticao/index.js
@@ -1,5 +1,21 @@
+/*
+console.log('Line 00')
 console.log('Line 01')
 console.log('Line 02')
 console.log('Line 03')
 console.log('Line 04')
 console.log('Line 05')
+*/
+
+// i = index
+
+/*
+for (let i = 0; i <= 500; i += 10) {
+  console.log(`Line ${i}`)
+}
+*/
+
+// DECRESENTE
+for (let i = 500; i >= 400; i -= 10) {
+  console.log(`Line ${i}`)
+}
