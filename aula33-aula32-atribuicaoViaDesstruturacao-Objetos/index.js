40038564b731c81dfd0569b08be6f2f1949bfef0
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 08:25:15 -0300

    atribuicaoViaDesstruturacao-Objetos.002

--- aula33-aula32-atribuicaoViaDesstruturacao-Objetos/index.js
+++ aula33-aula32-atribuicaoViaDesstruturacao-Objetos/index.js
@@ -4,9 +4,14 @@ const pessoa = {
   idade: 40,
   endereco: {
     rua: 'Av. Guilherme Gonçalves do Rosário',
-    numero: 326
+    numero: 326,
+    bairro: 'Parque Jacaranda'
   }
 }
 
-const nome = pessoa.nome
-console.log(pessoa.nome)
+// Atribuição via desestruturação
+const {
+  endereco: { rua, numero },
+  endereco
+} = pessoa
+console.log(rua, numero, endereco)
