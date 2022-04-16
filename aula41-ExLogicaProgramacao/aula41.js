8eab2d25bbd42b5f155ce4da899511f32babb424
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-16 18:31:13 -0300

    ExLogicaProgramacao.002

--- aula41-ExLogicaProgramacao/aula41.js
+++ aula41-ExLogicaProgramacao/aula41.js
@@ -1,1 +1,37 @@
 // Escreva uma função que recebe 02 númeors e retorne o maior deles
+
+function max(x, y) {
+  if (x > y) {
+    return x
+  } else {
+    return y
+  }
+}
+
+/*
+const maior = max(1, 2)
+console.log(maior)
+// ou console.log(max(1,2))
+*/
+
+/*********************************************/
+
+/*
+function max(x, y) {
+  if (x > y) {
+    return x
+  }
+  return y
+}
+
+console.log(max(10, 2))
+*/
+
+/*********************************************/
+
+function max(x, y) {
+  if (x > y) return x
+  return y
+}
+
+console.log(max(10, 2))
