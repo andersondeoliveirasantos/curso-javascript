166a0c000fa6af29b9417bde6b3caf278c24092f
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 23:27:32 -0300

    ForOf.003

--- aula37-ForOf/index.js
+++ aula37-ForOf/index.js
@@ -1,21 +1,30 @@
+const pessoa = {
+  nome: 'Anderson',
+  sobrenome: 'de Oliveira Santos'
+}
+
+for (let key in pessoa) {
+  console.log(key, pessoa[key])
+}
+
 /*
 const nome = ['Anderson', 'de', 'Oliveira', 'Santos']
 
-// CLASSICO
+// FOR CLASSICO - Geralmente com interáveis ( array ou strings)
 for (let i = 0; i < nome.length; i++) {
   console.log(nome[i])
 }
 
 console.log('############') // Diferenciar as funções
 
-// IN
+// FOR IN - Retorna o índice ou chave ( string, array ou objetos )
 for (let i in nome) {
   console.log(nome[i])
 }
 
 console.log('############') // Diferenciar as funções
 
-// OF
+// FOR OF - Retorna o valor em si ( interáveis, array ou strings )
 for (let valor of nome) {
   console.log(valor)
 }
