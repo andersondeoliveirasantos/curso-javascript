bde435e50573d3f8fc5fc694d6530470c2825f39
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-16 16:55:07 -0300

    whileDoWhile.001

--- aula38-ExNodeList/assets/js/main.js
+++ aula38-ExNodeList/assets/js/main.js
@@ -2,7 +2,10 @@ const paragrafos = document.querySelector('.paragrafos')
 const ps = paragrafos.querySelectorAll('p')
 
 const estilosBody = getComputedStyle(document.body)
+const backgroundColorBody = estilosBody.backgroundColor
+console.log(backgroundColorBody)
 
 for (let p of ps) {
-  console.log(p)
+  p.style.backgroundColor = backgroundColorBody
+  p.style.color = '#ffffff'
 }
---
+++ aula39-WhileDoWhile/index.js
@@ -0,0 +1,10 @@
+const nome = 'Anderson'
+
+let i = 0
+
+while (i < nome.length) {
+  console.log(nome[i])
+  i++
+}
+
+console.log('Follow the life ...')
