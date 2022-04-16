8df1bece4408c6b2d4d9903df8fcb0e1816d516e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 23:20:48 -0300

    ForOf.002

--- aula37-ForOf/index.js
+++ aula37-ForOf/index.js
@@ -1,17 +1,28 @@
+/*
 const nome = ['Anderson', 'de', 'Oliveira', 'Santos']
 
+// CLASSICO
 for (let i = 0; i < nome.length; i++) {
   console.log(nome[i])
 }
 
 console.log('############') // Diferenciar as funções
 
+// IN
 for (let i in nome) {
   console.log(nome[i])
 }
 
 console.log('############') // Diferenciar as funções
 
+// OF
 for (let valor of nome) {
   console.log(valor)
 }
+
+console.log('############') // Diferenciar as funções
+
+nome.forEach(function (valor, indice, array) {
+  console.log(valor, indice, array)
+})
+*/
