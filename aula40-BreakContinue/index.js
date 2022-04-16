d682e642e35af8003ceec88d35a43165f0ea00c5
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-16 17:32:39 -0300

    breakContinue.002

--- aula40-BreakContinue/index.js
+++ aula40-BreakContinue/index.js
@@ -1,12 +1,12 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for (let number of numbers) {
-  // Pula o número e passa para o próximo
+  // Continue - pula o número e passa para o próxima interação
   if (number === 2 || number === 5) {
     continue
   }
   console.log(number)
-
+  // Break - sai do laço
   if (number === 7) {
     console.log('7 encontrado, saindo da busca ...')
     break
