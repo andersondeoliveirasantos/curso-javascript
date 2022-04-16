cee2be00d33668b460518ae00b9f54624f34e093
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 22:19:07 -0300

    ForClassicoEstruturaDeRepeticao.007

--- aula35-demo/assets/js/main.js
+++ aula35-demo/assets/js/main.js
@@ -4,3 +4,17 @@ const elementos = [
   { tag: 'footer', texto: 'Phrase 03' },
   { tag: 'section', texto: 'Phrase 04' }
 ]
+
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+
+for (let i = 0; i < elementos.length; i++) {
+  let { tag, texto } = elementos[i]
+  let tagCriada = document.createElement(tag)
+  let textoCriado = document.createTextNode(texto)
+
+  tagCriada.appendChild(textoCriado)
+  div.appendChild(tagCriada)
+}
+
+container.appendChild(div)
