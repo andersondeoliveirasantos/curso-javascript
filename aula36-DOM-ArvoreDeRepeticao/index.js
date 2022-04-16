48c2b9afb1b2f455fb5a828cc56bde0dff060cdc
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 22:44:34 -0300

    DOM.ArvoreDeRepeticao.002

--- aula36-DOM-ArvoreDeRepeticao/index.js
+++ aula36-DOM-ArvoreDeRepeticao/index.js
@@ -1,11 +1,30 @@
-// for in -> Lê os índices ou chaves
+// for in -> Lê os índices ou chaves do objeto
 //                1       2       3
+
+const pessoa = {
+  nome: 'Anderson',
+  sobrenome: 'de Oliveira Santos',
+  idade: 30
+}
+
+for (let keys in pessoa) {
+  console.log(keys, pessoa[keys])
+}
+
+/********************************************/
+/*
 const fruits = ['Pera', 'Uva', 'Maçã']
 
-//for (let i = 0; i < fruits.length; i++) {
-//  console.log(fruits[i])
-//}
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i])
+}
+*/
+
+/********************************************/
+/*
+const fruits = ['Pera', 'Uva', 'Maçã']
 
 for (let indice in fruits) {
   console.log(fruits[indice])
 }
+*/
