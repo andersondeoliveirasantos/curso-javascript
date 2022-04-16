d2b9fb9f343a0a6b99fc5c79e776b4ac37768c7b
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-15 22:35:06 -0300

    DOM.ArvoreDeRepeticao.001

---
+++ aula36-DOM-ArvoreDeRepeticao/index.js
@@ -0,0 +1,11 @@
+// for in -> Lê os índices ou chaves
+//                1       2       3
+const fruits = ['Pera', 'Uva', 'Maçã']
+
+//for (let i = 0; i < fruits.length; i++) {
+//  console.log(fruits[i])
+//}
+
+for (let indice in fruits) {
+  console.log(fruits[indice])
+}
