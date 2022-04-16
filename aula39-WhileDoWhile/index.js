291bccc6b2a0d58ee4a94cac510f1a90fc417840
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-16 17:10:10 -0300

    whileDoWhile.002

--- aula39-WhileDoWhile/index.js
+++ aula39-WhileDoWhile/index.js
@@ -1,3 +1,27 @@
+// While
+function random(min, max) {
+  const r = Math.random() * (max - min) + min
+  return Math.floor(r)
+}
+
+const min = 1
+const max = 50
+let rand = random(min, max)
+
+while (rand !== 10) {
+  rand = random(min, max)
+  console.log(rand)
+}
+
+console.log('#################')
+
+// Do While
+do {
+  rand = random(min, max)
+  console.log(rand)
+} while (rand !== 10)
+
+/*
 const nome = 'Anderson'
 
 let i = 0
@@ -8,3 +32,4 @@ while (i < nome.length) {
 }
 
 console.log('Follow the life ...')
+*/
