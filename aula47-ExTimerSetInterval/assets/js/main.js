1ec5281112acc9db0a4be4539790567fc4c4d6e1
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-17 19:31:51 -0300

    ExTimerSetInterval.004

--- aula47-ExTimerSetInterval/assets/js/main.js
+++ aula47-ExTimerSetInterval/assets/js/main.js
@@ -1,4 +1,19 @@
 const relogio = document.querySelector('.relogio')
+
 const iniciar = document.querySelector('.iniciar')
+
 const pausar = document.querySelector('.pausar')
+
 const zerar = document.querySelector('.zerar')
+
+iniciar.addEventListener('click', function (event) {
+  alert('Cliquei no inicar !!!')
+})
+
+pausar.addEventListener('click', function (event) {
+  alert('Cliquei no pausar !!!')
+})
+
+zerar.addEventListener('click', function (event) {
+  alert('Cliquei no zerar !!!')
+})
