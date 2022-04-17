94dd942680462ec948850d3409e82b8eea41e4a5
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-17 17:49:14 -0300

    TryCatchFinally.003

--- aula45-TryCatchFinally/aula45.js
+++ aula45-TryCatchFinally/aula45.js
@@ -1,7 +1,17 @@
+/*
 try {
   // É executado quando não há erros
 } catch (e) {
   // é executado quando há erros
 } finally {
   // Sempre é executado
+}
+*/
+
+try {
+  console.log('Abri um arquivo')
+  console.log('Manipulei o arquivo e gerou erro')
+  console.log('Fechei o arquivo')
+} catch (e) {
+} finally {
 }
