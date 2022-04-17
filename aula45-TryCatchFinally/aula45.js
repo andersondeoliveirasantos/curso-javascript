4d2bcf69ca9d589a3745aafc81f25d38dc64580c
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-17 18:10:43 -0300

    TryCatchFinally.005

--- aula45-TryCatchFinally/aula45.js
+++ aula45-TryCatchFinally/aula45.js
@@ -7,7 +7,8 @@ try {
   // Sempre é executado
 }
 */
-
+/****************************************************/
+/*
 try {
   console.log('Abri um arquivo')
   console.log('Manipulei o arquivo e gerou erro')
@@ -15,4 +16,26 @@ try {
 } catch (e) {
   console.log('Tratando o erro')
 } finally {
+  console.log('Eu sempre sou executado')
 }
+*/
+
+function retornaHora(data) {
+  if (data && !(data instanceof Date)) {
+    throw new TypeError('Esperando instância de Date.')
+  }
+
+  if (!data) {
+    data = new Date()
+  }
+
+  return data.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  })
+}
+
+const hora = retornaHora()
+console.log(hora)
