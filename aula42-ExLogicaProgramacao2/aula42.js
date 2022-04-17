ad8c5af6d5762dd6903da91d0f44a64c69a69003
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-16 21:53:50 -0300

    ExLogicaProgramacao.008

--- aula42-ExLogicaProgramacao2/aula42.js
+++ aula42-ExLogicaProgramacao2/aula42.js
@@ -4,8 +4,20 @@
 // Retorne true se a imagem estiver no
 // modo paisagem.
 
+/*****************************************************/
+/*
+function ePaisagem(largura, altura) {
+  return largura > altura
+}
+
+console.log(ePaisagem(1920, 1920))
+*/
+
+/*****************************************************/
+/*
 function ePaisagem(largura, altura) {
   return largura > altura ? true : false
 }
 
 console.log(ePaisagem(1080, 1920))
+*/
