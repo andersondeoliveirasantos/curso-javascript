270e71c567da2c54d633cf82470ca07929970657
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-21 18:52:01 -0300

    s4.ManeirasDeDeclararFuncoes.002

--- S4-aula49-DeclararFuncoes/aula49.js
+++ S4-aula49-DeclararFuncoes/aula49.js
@@ -12,3 +12,17 @@ const souUmDado = function () {
   console.log('Sou um dado.')
 }
 souUmDado()
+
+// Arrow function
+const funcaoArrow = () => {
+  console.log('Sou uma função Arrow function')
+}
+funcaoArrow()
+
+// Dentro de um objeto
+const obj = {
+  falar() {
+    console.log('Shi is speaking ...')
+  }
+}
+obj.falar()
