4c371ea7cd9dc5b527b4da40c3021a28d959f9ec
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-21 19:21:10 -0300

    S4-ParametrosDaFuncao.002

--- aula50-S4-ParametrosDaFuncao/aula50.js
+++ aula50-S4-ParametrosDaFuncao/aula50.js
@@ -8,9 +8,20 @@ function funcao() {
 }
 funcao(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
+console.log('__________________')
+console.log(' ')
 /*****************************************************/
 
 function funcao2(a, b, c, d, e, f) {
   console.log(a, b, c, d, e, f)
 }
 funcao2(1, 2, 3)
+
+console.log('_________________')
+console.log(' ')
+/*****************************************************/
+
+function funcao3(a, b) {
+  console.log(a + b)
+}
+funcao3(2)
