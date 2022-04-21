4c83966c62d9ffdb262a4ffef81592c1892ce4fc
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-21 19:08:54 -0300

    S4-ParametrosDaFuncao.001

---
+++ aula50-S4-ParametrosDaFuncao/aula50.js
@@ -0,0 +1,16 @@
+// Argumentos que sustenta todos os argumentos enviados
+function funcao() {
+  let total = 0
+  for (let argumento of arguments) {
+    total += argumento
+  }
+  console.log(total)
+}
+funcao(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+/*****************************************************/
+
+function funcao2(a, b, c, d, e, f) {
+  console.log(a, b, c, d, e, f)
+}
+funcao2(1, 2, 3)
