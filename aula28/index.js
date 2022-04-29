d9c22d60bbb0cfe8a99f17ef5cf8732e1eaeba41
Anderson de Oliveira Santos <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-04-28 22:06:52 -0300

    Test.001

--- aula28/index.js
+++ aula28/index.js
@@ -1,11 +1,8 @@
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 // const tresHoras = 60 * 60 * 3 8 1000 // Equivale a três horas
-
 // const umDia = 60 * 60 * 24 * 1000
-
 // const data = new Date(0 + umDia) // 01/01/1970 Timestamp unix ou época unix
-
 // const data = new Date(2022, 3, 20, 15, 14, 27, 500) // Ano, Mês, Dia, Hora, Minuto, Segundo, Milésimo de Segundo
 
 /*
