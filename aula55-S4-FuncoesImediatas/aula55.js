414cb6de3e1488ef168fa208577e95073301841f
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-30 13:07:05 -0300

    S4-FuncoesImediatas.002

--- aula55-S4-FuncoesImediatas/aula55.js
+++ aula55-S4-FuncoesImediatas/aula55.js
@@ -1,6 +1,15 @@
 // IIFE - Immediately Invoked Function Expression
-function qualquerCoisa() {
-  console.log(123456789)
-}
 
-qualquerCoisa()
+;(function (idade, peso, altura) {
+  const sobrenome = 'de Oliveira Santos'
+  function criaNome(nome) {
+    return nome + ' ' + sobrenome
+  }
+
+  function falaNome() {
+    console.log(criaNome('Anderson'))
+  }
+
+  falaNome()
+  console.log(idade, peso, altura)
+})(30, 80, 1.8)
