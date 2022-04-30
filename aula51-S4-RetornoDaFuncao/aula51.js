a470f896d7aca1516919baab4e1dce249b915d0b
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-30 11:41:40 -0300

    S4-RetornoDaFuncao.015

--- aula51-S4-RetornoDaFuncao/aula51.js
+++ aula51-S4-RetornoDaFuncao/aula51.js
@@ -12,3 +12,43 @@ function falaFrase(comeco) {
 const fala = falaFrase('Hello')
 const resto = fala('world !!!')
 console.log(resto)
+
+console.log('___________________________________')
+console.log(' ')
+
+// Função simples:
+/*
+function duplica(n) {
+  return n * 2
+}
+
+function triplica(n) {
+  return n * 3
+}
+
+function quadriplica(n) {
+  return n * 4
+}
+
+console.log(duplica(2))
+console.log(triplica(2))
+console.log(quadriplica(2))
+*/
+
+console.log('___________________________________')
+console.log(' ')
+
+function criaMultiplicador(multiplicador) {
+  // Multiplicador
+  return function (n) {
+    return n * multiplicador
+  }
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+
+console.log(duplica(3))
+console.log(triplica(2))
+console.log(quadriplica(10))
