46da52568514aba1e6c81f6e2e9a40b344362676
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-30 12:20:54 -0300

    S4-FuncoesDeCallback.002

--- aula54-S4-FuncoesDeCallback/aula54.js
+++ aula54-S4-FuncoesDeCallback/aula54.js
@@ -1,11 +1,23 @@
 function f1() {
-  console.log(f1)
+  setTimeout(function () {
+    console.log('F1')
+  }, 500)
 }
 
 function f2() {
-  console.log(f2)
+  setTimeout(function () {
+    console.log('F2')
+  }, 1000)
 }
 
 function f3() {
-  console.log(f3)
+  setTimeout(function () {
+    console.log('F3')
+  }, 800)
 }
+
+f1()
+f2()
+f3()
+
+console.log('Hello World !!!')
