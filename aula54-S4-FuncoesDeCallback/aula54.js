dfa3ddd7f970c5369328c7692a9dafebb4640551
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-04-30 12:23:59 -0300

    S4-FuncoesDeCallback.003

--- aula54-S4-FuncoesDeCallback/aula54.js
+++ aula54-S4-FuncoesDeCallback/aula54.js
@@ -1,3 +1,10 @@
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min
+  return Math.floor(num)
+}
+
+console.log(rand())
+
 function f1() {
   setTimeout(function () {
     console.log('F1')
