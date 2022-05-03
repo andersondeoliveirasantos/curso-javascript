33559a768d169cf4b6dd6dd002bce2d255bd6bc2
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-02 21:08:36 -0300

    S4-FactoryFunctions.009

--- aula56-S4-FactoryFunctions/factory/factory.js
+++ aula56-S4-FactoryFunctions/factory/factory.js
@@ -7,18 +7,18 @@ function criaPessoa(nome, sobrenome, a, p) {
 
     // Getter
     get nomeCompleto() {
-      return `${this.nome} ${this.sobrenome}`;
+      return `${this.nome} ${this.sobrenome}`
     },
 
     // Setter
     set nomeCompleto(valor) {
-      valor = valor.split(' ');
-      this.nome = valor.shift();
-      this.sobrenome = valor.join(' ');
+      valor = valor.split(' ')
+      this.nome = valor.shift()
+      this.sobrenome = valor.join(' ')
     },
 
     fala(assunto = 'falando sobre NADA') {
-      return `${this.nome} está ${assunto}.`;
+      return `${this.nome} está ${assunto}.`
     },
 
     altura: a,
@@ -26,17 +26,17 @@ function criaPessoa(nome, sobrenome, a, p) {
 
     // Getter
     get imc() {
-      const indice = this.peso / (this.altura ** 2);
-      return indice.toFixed(2);
+      const indice = this.peso / this.altura ** 2
+      return indice.toFixed(2)
     }
-  };
+  }
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
-const p2 = criaPessoa('João', 'Otávio', 1.90, 57);
-const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80)
 
-console.log(p1.imc);
-console.log(p2.imc);
-console.log(p3.imc);
+const p2 = criaPessoa('João', 'Otávio', 1.9, 57)
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110)
 
+console.log(p1.imc)
+console.log(p2.imc)
+console.log(p3.imc)
