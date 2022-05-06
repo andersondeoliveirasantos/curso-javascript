3abcce1385ff7edfa789d2657d6485af2d0af839
Anderson de Oliveira Santos <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-05-05 22:12:56 -0300

    operadores.002

--- aula21/index.js
+++ aula21/index.js
@@ -3,11 +3,11 @@
 OPERADORES DE COMPARAÇÃO !!!
 
 >   maior que 
-
 >=  maior que ou igual a 
 
 <   menor que 
 <=  menor que ou igual a  
+
 ==  igualdade ( valor ) *************
 === igualdade estrita ( valor e tipo )
 !=  diferente ( valor ) *************
