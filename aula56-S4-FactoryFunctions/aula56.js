4c735d67d6b327a1b3cefb922240a2981dd2f94a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-06 23:34:37 -0300

    S4-FactoryFunctions.016

--- aula56-S4-FactoryFunctions/aula56.js
+++ aula56-S4-FactoryFunctions/aula56.js
@@ -19,5 +19,7 @@ const p1 = criaPessoa('Anderson', 'de Oliveira Santos', 1.8, 80)
 console.log(p1.imc())
 
 const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 42)
+
 console.log(p1.fala('falando sobre JavaSript'))
+
 console.log(p2.imc())
