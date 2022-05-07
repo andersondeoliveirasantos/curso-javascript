a15eb5c06ab26eb4da601f777326a27c491ebe21
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-07 05:55:24 -0300

    PraticandoFactoryFunctions.001

--- aula56-S4-FactoryFunctions/aula56.js
+++ aula56-S4-FactoryFunctions/aula56.js
@@ -1,13 +1,30 @@
+// Factory function ( Função fábrica )
 function criaPessoa(nome, sobrenome, a, p) {
   return {
     nome,
     sobrenome,
-    fala: function (assunto) {
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ')
+      this.nome = valor.shift()
+      this.sobrenome = valor.join(' ')
+      console.log(valor)
+    },
+
+    fala(assunto = 'falando -> How are you?') {
       return `${this.nome} está ${assunto}`
     },
     altura: a,
     peso: p,
-    imc() {
+
+    // Getter
+    get imc() {
       const indice = this.peso / this.altura ** 2
       return indice.toFixed(2)
     }
@@ -15,11 +32,13 @@ function criaPessoa(nome, sobrenome, a, p) {
 }
 
 const p1 = criaPessoa('Anderson', 'de Oliveira Santos', 1.8, 80)
+console.log(p1.nome)
+console.log(p1.sobrenome)
+console.log(p1.imc)
 
-console.log(p1.imc())
+console.log(p1.nomeCompleto)
 
-const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 42)
-
-console.log(p1.fala('falando sobre JavaSript'))
-
-console.log(p2.imc())
+p1.nomeCompleto = 'Maria Oliveira Silva'
+console.log(p1.nome)
+console.log(p1.sobrenome)
+console.log(p1.fala())
