5747d465502cabe0a1fcd8564be633516e6b6400
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-08 07:19:12 -0300

    ConstructorFunctions.004

--- aula58-S4-ConstructorFunctions/aula58.js
+++ aula58-S4-ConstructorFunctions/aula58.js
@@ -2,8 +2,17 @@
 // Função fabrica -> objetos
 // Construtora -> Pessoaaa ( new )
 function Pessoa(nome, sobrenome) {
+  // Privadas
+  const ID = 123456
+  const metodoInterno = function () {}
+
+  // Atributos ou métodos públicos
   this.nome = nome
   this.sobrenome = sobrenome
+
+  this.metodo = function () {
+    console.log(this.nome + ':sou um método')
+  }
 }
 
 const p1 = new Pessoa('Anderson', 'de Oliveira Santos')
