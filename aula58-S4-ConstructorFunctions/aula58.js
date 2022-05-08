374819619883dd29ced0274b228432a8dbe72a64
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-08 07:13:17 -0300

    ConstructorFunctions.003

--- aula58-S4-ConstructorFunctions/aula58.js
+++ aula58-S4-ConstructorFunctions/aula58.js
@@ -5,3 +5,6 @@ function Pessoa(nome, sobrenome) {
   this.nome = nome
   this.sobrenome = sobrenome
 }
+
+const p1 = new Pessoa('Anderson', 'de Oliveira Santos')
+const p2 = new Pessoa('Alice', 'Silva')
