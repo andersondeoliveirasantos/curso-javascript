be722ee4934143a7e27166420060f9ad76bd6ec6
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-08 06:00:53 -0300

    PraticandoFactoryFunctions.004

--- aula57-S4-PraticandoFactoryFunctions/assets/js/main.js
+++ aula57-S4-PraticandoFactoryFunctions/assets/js/main.js
@@ -2,23 +2,18 @@ function criaCalculadora() {
   return {
     display: document.querySelector('.display'),
 
-
-
-
-
-
     inicia() {
       this.cliqueBotoes()
     },
     cliqueBotoes() {
       // this -> calculadora
-      document.addEventListener('click', function(e) {
+      document.addEventListener('click', e => {
         const el = e.target
 
         if(el.classList.contains('btn-num')) {
         this.btnParaDisplay(el.innerText)
       }
-    }.bind(this))
+    })
   },
 
   btnParaDisplay(valor) {
