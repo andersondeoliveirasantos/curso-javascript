aff3cfa4208c97e004e22e058a120df92267b817
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-15 06:55:54 -0300

    FuncoesRecursivas.003

--- aula61-S4-FuncoesGeradoras/aula61.js
+++ aula61-S4-FuncoesGeradoras/aula61.js
@@ -8,7 +8,13 @@ function* geradora1() {
 }
 
 const g1 = geradora1()
+for (let valor of g1) {
+  console.log(valor)
+}
+
+/*
 console.log(g1.next().value)
 console.log(g1.next().value)
 console.log(g1.next())
 console.log(g1.next())
+*/
