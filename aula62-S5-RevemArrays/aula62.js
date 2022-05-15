c31ee4bbac1cb077517de64f1453a2d9c5ad2c62
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-15 07:38:59 -0300

    RevEmArrays.002

--- aula62-S5-RevemArrays/aula62.js
+++ aula62-S5-RevemArrays/aula62.js
@@ -1,4 +1,14 @@
+// Valor por referência
 //                 1         2        3
+/*
 const nomes = ['Eduardo', 'Maria', 'Joana']
 nomes[2] = 'João'
 console.log(nomes)
+*/
+
+// const nome = 'Anderson Oliveira Santos'
+// const nomes = nome.split(' ') // Ou separar por virgula.
+
+const nomes = ['Anderson', 'Oliveira', 'Santos']
+const nome = nomes.join(' ')
+console.log(nome)
