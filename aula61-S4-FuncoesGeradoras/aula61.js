09b60d1824a4b6b1226bf335abee9a976c9b81c9
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-15 07:17:54 -0300

    FuncoesRecursivas.008

--- aula61-S4-FuncoesGeradoras/aula61.js
+++ aula61-S4-FuncoesGeradoras/aula61.js
@@ -30,9 +30,36 @@ function* geradora4() {
 }
 
 const g4 = geradora4()
+/*
 for (let valor of g4) {
   console.log(valor)
 }
+*/
+
+function* geradora5() {
+  yield function () {
+    console.log('Vim do yield 1')
+  }
+
+  yield function () {
+    console.log('Vim do retorn')
+  }
+
+  // ...
+
+  yield function () {
+    console.log('Vim do yield 2')
+  }
+}
+
+const g5 = geradora5()
+const func1 = g5.next().value
+const func2 = g5.next().value
+const func3 = g5.next().value
+
+func1()
+func2()
+func3()
 
 /*
 const g2 = geradora2()
