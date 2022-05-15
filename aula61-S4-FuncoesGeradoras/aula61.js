18cfbc2a1d8818825523f905ce17aa50120fb96f
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-15 06:59:18 -0300

    FuncoesRecursivas.004

--- aula61-S4-FuncoesGeradoras/aula61.js
+++ aula61-S4-FuncoesGeradoras/aula61.js
@@ -7,10 +7,26 @@ function* geradora1() {
   yield 'Valor 3'
 }
 
+function* geradora2() {
+  let i = 0
+
+  while (true) {
+    yield i
+    i++
+  }
+}
+
+const g2 = geradora2()
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+
+/*
 const g1 = geradora1()
 for (let valor of g1) {
   console.log(valor)
 }
+*/
 
 /*
 console.log(g1.next().value)
