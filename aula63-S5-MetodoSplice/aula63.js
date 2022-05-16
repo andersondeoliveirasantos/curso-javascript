420a9bd8fe0a7eb4efb4834d76db4c5f6c3287d9
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-16 04:50:50 -0300

    MetodoSplice.003

--- aula63-S5-MetodoSplice/aula63.js
+++ aula63-S5-MetodoSplice/aula63.js
@@ -1,10 +1,19 @@
 //               -5        -4       -3        -2        -1
 //               0         1        2          3         4
 const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
-
-// nomes.splice(indice, delete, elem1, elem2, elem3)
-// pop
-// const removidos = nomes.splice(-2, 2)
-const removidos = nomes.splice(-2, Number.MAX_VALUE) // Vai até o ultimo item do Array
 console.log(nomes, removidos)
 console.log(Number.MAX_VALUE) // Maior número no JavaScript
+
+// Descrição da função:
+// nomes.splice(indice, delete, elem1, elem2, elem3)
+
+// pop
+// const removidos = nomes.splice(-2, 2)
+
+//const removidos = nomes.splice(-2, Number.MAX_VALUE) // Vai até o ultimo item do Array
+
+// Remove três e adiciona 02:
+//const removidos = nomes.splice(3, 3, 'Anderson', 'Oliveira')
+
+// shit
+//const removidos = nomes.splice(0, 1)
