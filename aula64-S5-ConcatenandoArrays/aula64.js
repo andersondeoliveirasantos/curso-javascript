a1dfa27a5db5905e433da6d6d5f35c7170accd51
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-28 23:31:08 -0300

    ConcatenandoArrays.004

--- aula64-S5-ConcatenandoArrays/aula64.js
+++ aula64-S5-ConcatenandoArrays/aula64.js
@@ -2,4 +2,5 @@ const a1 = [1, 2, 3]
 const a2 = [4, 5, 6]
 // const a3 = a1.concat(a2, [7, 8, 9], 'Anderson')
 // ... rest -> ... spread
-const a3 = console.log(a3)
+const a3 = [...a1, 'Anderson', ...a2]
+console.log(a3)
