879384bcadcae836a13477af303607f8c541956c
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-29 06:07:42 -0300

    FiltrandoArray.002

--- aula65-S5-Filter-FiltrandoArray/aula65.js
+++ aula65-S5-Filter-FiltrandoArray/aula65.js
@@ -4,12 +4,14 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
-function callbackFilter(valor, indice, array) {
-  if (valor > 10) {
-    return true
-  } else {
-    return false
-  }
+/*
+function callbackFilter(valor) {
+  return valor > 10
 }
+*/
 
-const numerosFiltrados = numeros.filter()
+const numerosFiltrados = numeros.filter(function (valor) {
+  return valor > 10
+})
+
+console.log(numerosFiltrados)
