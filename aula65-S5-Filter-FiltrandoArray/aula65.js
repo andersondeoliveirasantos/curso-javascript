9c102d446026b9abb225b4c0106e845f7ef792c4
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-29 06:01:25 -0300

    FiltrandoArray.001

---
+++ aula65-S5-Filter-FiltrandoArray/aula65.js
@@ -0,0 +1,15 @@
+// Filter, map, reduce
+
+// Retorne os números maiores que 10
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+function callbackFilter(valor, indice, array) {
+  if (valor > 10) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numerosFiltrados = numeros.filter()
