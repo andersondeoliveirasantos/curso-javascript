8c88f3f6e5917225c6c07364e5c9bcedd69786fd
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-29 06:56:27 -0300

    MapeandoArray.003

--- aula66-S5-Map-MapeandoArray/aula66.js
+++ aula66-S5-Map-MapeandoArray/aula66.js
@@ -1,6 +1,5 @@
 // Dobre os números
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const numerosEmDobro = numeros.map(function (valor, indice, array) {
-  console.log(valor, indice, array)
-})
+const numerosEmDobro = numeros.map(valor => valor * 2)
+console.log(numerosEmDobro)
