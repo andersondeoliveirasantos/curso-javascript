ba669e93375c84bd35ca5db3231cf1f09eab9abd
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-05-29 06:27:54 -0300

    FiltrandoArray.003

--- aula65-S5-Filter-FiltrandoArray/aula65.js
+++ aula65-S5-Filter-FiltrandoArray/aula65.js
@@ -1,8 +1,8 @@
-// Filter, map, reduce
+// Filter -> Sempre retorna um array, com a mesma quantidade de elementos ou menos
 
 // Retorne os números maiores que 10
 
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 /*
 function callbackFilter(valor) {
@@ -10,8 +10,43 @@ function callbackFilter(valor) {
 }
 */
 
+/* // Completa
 const numerosFiltrados = numeros.filter(function (valor) {
   return valor > 10
 })
+*/
 
+/* // Código reduzido
+const numerosFiltrados = numeros.filter(valor => valor > 10)
 console.log(numerosFiltrados)
+*/
+
+// ********************************
+
+/* Códig completo:
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosFiltrados = numeros.filter((valor, indice, array) => {
+  console.log(valor, indice, array)
+  return valor > 10
+})
+console.log(numerosFiltrados)
+*/
+
+// Código reduzido
+/*
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosFiltrados = numeros.filter(valor => valor > 10)
+console.log(numerosFiltrados)
+*/
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+  {nome: 'Anderson', idade: 62}
+  {nome: 'Maria', idade: 23}
+  {nome: 'Eduardo', idade: 55}
+  {nome: 'Letícia', idade: 19}
+  {nome: 'Rosana', idade: 32}
+  {nome: 'Wallace', idade: 47}
+]
