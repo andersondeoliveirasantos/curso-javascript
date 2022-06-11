7f3782bc0b0ccd743d13034b2178663160bce28a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-10 22:56:37 -0300

    ReduzindoArray.017

--- aula67-S5-Reduce-ReduzindoArray/aula67.js
+++ aula67-S5-Reduce-ReduzindoArray/aula67.js
@@ -1,9 +1,5 @@
-const nome1 = 123
-const nome2 = 456
-const nome3 = 789
-const nome4 = 0
+// Some todos os números ( Reduce )
+// Retorne um array com os pares ( Filter )
+// Retorne um array com o dobro dos valores ( Map )
 
-console.log(nome1)
-console.log(nome2)
-console.log(nome3)
-console.log(nome4)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
