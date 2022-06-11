6a3092c8ee71681aa0062021138c2a0b5dfdbce5
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-10 23:02:49 -0300

    ReduzindoArray.018

--- aula67-S5-Reduce-ReduzindoArray/aula67.js
+++ aula67-S5-Reduce-ReduzindoArray/aula67.js
@@ -3,3 +3,10 @@
 // Retorne um array com o dobro dos valores ( Map )
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const total = numeros.reduce(function (acumulador, valor, indica, array) {
+  acumulador += valor
+  return acumulador
+}, 0)
+
+console.log(total)
