957abc5097d57b563b246c3d587f0eb7b83d721e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-13 22:46:22 -0300

    ReduzindoArray.022

--- aula67-S5-Reduce-ReduzindoArray/aula67.js
+++ aula67-S5-Reduce-ReduzindoArray/aula67.js
@@ -23,5 +23,6 @@ const pessoas = [
 ]
 
 const maisVelha = pessoas.reduce(function (acumulador, valor) {
-  console.log(acumulador, valor)
+  if (acumulador.idade > valor.idade) return acumulador
+  return valor
 })
