55c4e7b28760780f8e13e697a4c010e06037c17e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-13 22:52:44 -0300

    FilterMap Reduce.001

---
+++ aula68-S5-FilterMapReduce/aula68.js
@@ -0,0 +1,6 @@
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir ( soma tudo )
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
