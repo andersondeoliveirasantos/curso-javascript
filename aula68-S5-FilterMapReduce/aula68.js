83c615d1fcb6ca40d9363829de1eb211d8a17e47
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-15 22:49:28 -0300

    FilterMapReduce.002

--- aula68-S5-FilterMapReduce/aula68.js
+++ aula68-S5-FilterMapReduce/aula68.js
@@ -4,5 +4,8 @@
 // -> Reduzir ( soma tudo )
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosPares = numeros.filter(function (valor) {
+  return valor % 2 === 0
+})
 
-console.log(numeros)
+console.log(numerosPares)
