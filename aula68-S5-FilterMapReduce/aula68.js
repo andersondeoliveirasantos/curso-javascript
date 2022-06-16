ac1a7bddcece5640f6ebbf434b3e769ad16deabe
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-15 22:58:10 -0300

    FilterMapReduce.003

--- aula68-S5-FilterMapReduce/aula68.js
+++ aula68-S5-FilterMapReduce/aula68.js
@@ -4,8 +4,11 @@
 // -> Reduzir ( soma tudo )
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const numerosPares = numeros.filter(function (valor) {
-  return valor % 2 === 0
-})
+const numerosPares = numeros
+  .filter(valor => valor % 2 === 0)
+  .map(valor => valor * 2)
+  .reduce((ac, valor) => ac + valor)
 
+// [ 50,   80, 2,  8, 22 ] Pares
+// [ 100, 160, 4, 16, 44 ] Dobro
 console.log(numerosPares)
