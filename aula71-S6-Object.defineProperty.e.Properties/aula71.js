497a9c505aaae54f4a86ebae4ac3e3bd3f7f04ca
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-16 17:49:15 -0300

    Object.defineProperty.e.Properties.002

--- aula71-S6-Object.defineProperty.e.Properties/aula71.js
+++ aula71-S6-Object.defineProperty.e.Properties/aula71.js
@@ -1,1 +1,10 @@
 // defineProperty - definePropeties
+function Produto(nome, preco, estoque) {
+  this.nome = nome
+  this.preco = preco
+
+  Object.defineProperty(this, 'estoque', {})
+}
+
+const p1 = new Produto('Camiseta', 20, 3)
+console.log(p1)
