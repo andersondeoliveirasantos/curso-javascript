aebbcbf289047150715a2821613915b70f9684fe
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-16 17:59:29 -0300

    Object.defineProperty.e.Properties.003

--- aula71-S6-Object.defineProperty.e.Properties/aula71.js
+++ aula71-S6-Object.defineProperty.e.Properties/aula71.js
@@ -3,7 +3,12 @@ function Produto(nome, preco, estoque) {
   this.nome = nome
   this.preco = preco
 
-  Object.defineProperty(this, 'estoque', {})
+  Object.defineProperty(this, 'estoque', {
+    enumerable: true, // Mostra a chave
+    value: estoque, // Valor
+    writable: false, // Pode alterar o valor ou não
+    configurable: true // Configurável
+  })
 }
 
 const p1 = new Produto('Camiseta', 20, 3)
