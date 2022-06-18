429ff34114146bcc2496fa683059ef07e8740cd8
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-18 06:20:46 -0300

    MétodosObjetos.003

--- aula73-S6-MétodosObjetos/aula73.js
+++ aula73-S6-MétodosObjetos/aula73.js
@@ -11,7 +11,20 @@ Object.defineProperties (define várias propriedades)
 Object.defineProperty (define uma propriedade)
 */
 
-const produto = { nome: 'Produto', preco: 1,8 }
+const produto = { nome: 'Produto', preco: 1.8 }
+Object.defineProperty(produto, 'nome', {
+  writable: false,
+  configurable: false,
+  value: 'Qualquer outra coisa'
+})
+
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
+console.log(produto)
+
+/*
+const produto = { nome: 'Produto', preco: 1.8 }
+Object.freeze(produto)  // Não deixa alterar a variável
 const caneca = { nome: produto.nome, preco: produto.preco }
 
-console.log(produto)
+console.log(Object.keys(produto))
+*/
