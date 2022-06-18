d845ff98bcfa16215ba3a042691d8c2ea9362a28
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-17 21:15:30 -0300

    MétodosObjetos.001

---
+++ aula73-S6-MétodosObjetos/aula73.js
@@ -0,0 +1,12 @@
+/*
+Object.values
+Object.entries
+object.getOwnPropertyDescriptor(o, 'prop)
+... (spread)
+
+// Já vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
