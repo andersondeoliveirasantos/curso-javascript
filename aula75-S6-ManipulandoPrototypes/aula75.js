3172b8fc9d2b909d0f93cc56f333e9111c6933e8
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-20 22:39:02 -0300

    ManipulandoPrototypes.003

--- aula75-S6-ManipulandoPrototypes/aula75.js
+++ aula75-S6-ManipulandoPrototypes/aula75.js
@@ -1,5 +1,23 @@
-// New object -> Object.prototype
+function Produto(nome, preco) {
+  this.nome = nome
+  this.preco = preco
+}
 
+Produto.prototype.desconto = function (percentual) {
+  this.preco = this.preco - this.preco * (percentual / 100)
+}
+
+Produto.prototype.aumento = function (percentual) {
+  this.preco = this.preco + this.preco * (percentual / 100)
+}
+
+const p1 = new Produto('Camiseta', 50)
+p1.desconto(100)
+p1.aumento(100)
+console.log(p1)
+
+// New object -> Object.prototype
+/*
 const objA = {
   chaveA: 'A'
   // __proto__: Object.prototype
@@ -16,3 +34,4 @@ objC.chaveC = 'C'
 Object.setPrototypeOf(objB, objA)
 Object.setPrototypeOf(objC, objB)
 console.log(objB.chaveA)
+*/
