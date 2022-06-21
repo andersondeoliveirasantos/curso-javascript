35b3f1273bc37655d8e1bc6f0f8cb213cd2c1cfe
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-20 22:27:35 -0300

    ManipulandoPrototypes.002

--- aula75-S6-ManipulandoPrototypes/aula75.js
+++ aula75-S6-ManipulandoPrototypes/aula75.js
@@ -1,9 +1,18 @@
 // New object -> Object.prototype
 
-const objA: 'A' = {
+const objA = {
+  chaveA: 'A'
   // __proto__: Object.prototype
 }
 
-const objB: 'B' = {
-  // __proto__: Object.prototype
+const objB = {
+  chaveB: 'B'
+  // __proto__: ObjA
 }
+
+const objC = new Object()
+objC.chaveC = 'C'
+
+Object.setPrototypeOf(objB, objA)
+Object.setPrototypeOf(objC, objB)
+console.log(objB.chaveA)
