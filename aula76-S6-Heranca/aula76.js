29ab26fce52eb3528836c461358571ce334e322a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-20 23:23:06 -0300

    Heranca.003

--- aula76-S6-Heranca/aula76.js
+++ aula76-S6-Heranca/aula76.js
@@ -22,10 +22,34 @@ function Camiseta(nome, preco, cor) {
 Camiseta.prototype = Object.create(Produto.prototype)
 Camiseta.prototype.constructor = Camiseta
 
-Camiseta.prototype.aumento
+Camiseta.prototype.aumento = function (percentual) {
+  this.preco = this.preco + this.preco * (percentual / 100)
+}
+
+function Caneca(nome, preco, material, estoque) {
+  Produto.call(this, nome, preco)
+  this.material = material
+  // this.estoque = estoque
+  Object.defineProperty(this, 'estoque', {
+    enumerable: true,
+    configurable: false,
+    get: function () {
+      return estoque
+    },
+    set: function (valor) {
+      if (typeof valor !== 'number') return
+      estoque = valor
+    }
+  })
+}
+
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
 
 const produto = new Produto('Gen', 111)
 const camiseta = new Camiseta('Regata', 7.5, 'Preta')
+const caneca = new Caneca('Caneca', 13, 'Plastico', 5)
 // camiseta.aumento(10)
-console.log(produto)
+console.log(caneca)
 console.log(camiseta)
+console.log(produto)
