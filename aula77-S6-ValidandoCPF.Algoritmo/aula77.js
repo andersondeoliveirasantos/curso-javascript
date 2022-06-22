a4cf35dec0e230f946a3185b32f1b42e5cd9c7da
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-21 23:52:34 -0300

    ValidandoCPF.Algoritmo.006

--- aula77-S6-ValidandoCPF.Algoritmo/aula77.js
+++ aula77-S6-ValidandoCPF.Algoritmo/aula77.js
@@ -43,20 +43,27 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function () {
   if (typeof this.cpfLimpo === 'undefined') return false
   if (this.cpfLimpo.length !== 11) return false
+
   const cpfParcial = this.cpfLimpo.slice(0, -2)
   const digito1 = this.criaDigito(cpfParcial)
+  const digito2 = this.criaDigito(cpfParcial + digito1)
+
   return true
 }
 
 ValidaCPF.prototype.criaDigito = function (cpfParcial) {
   const cpfArray = Array.from(cpfParcial)
+
   let regressivo = cpfArray.length + 1
-  const digito = cpfArray.reduce((ac, val) => {
-    console.log(regressivo, val, regressivo * val)
+  const total = cpfArray.reduce((ac, val) => {
+    ac += regressivo * Number(val)
     regressivo--
     return ac
   }, 0)
+
+  const digito = 11 - (total % 11)
+  return digito > 9 ? 0 : digito
 }
 
-const cpf = new ValidaCPF('705.484.450-52')
+const cpf = new ValidaCPF('070.987.720-03')
 console.log(cpf.valida())
