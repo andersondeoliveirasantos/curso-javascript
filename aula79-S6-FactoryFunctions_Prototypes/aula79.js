425af232a106a3b78c335fd5abe75866d15013a0
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-26 06:51:21 -0300

    FactoryFunctions_Prototypes.003

--- aula79-S6-FactoryFunctions_Prototypes/aula79.js
+++ aula79-S6-FactoryFunctions_Prototypes/aula79.js
@@ -1,22 +1,28 @@
-function criaPessoa(nome, sobrenome) {
-  return {
-    nome,
-    sobrenome
-
-    /*
-    falar() {
-      console.log(`${this.nome} está falando.`)
-    },
-
-    comer() {
-      console.log(`${this.nome} está comendo`)
-    },
-
-    beber() {
-      console.log(`${this.nome} está bebendo`)
-    }
-    */
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando`)
   }
+}
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo`)
+  }
+}
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo`)
+  }
+}
+
+const pessoaPrototype = { ...falar, ...comer, ...beber }
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  })
 }
 
 const p1 = criaPessoa('Anderson', 'de Oliveira Santos')
