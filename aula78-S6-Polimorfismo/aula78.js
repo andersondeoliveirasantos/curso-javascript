02dfad5fbcf278838fae390bca61c0655c32d7fd
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-25 23:01:22 -0300

    Polimorfismo.003

--- aula78-S6-Polimorfismo/aula78.js
+++ aula78-S6-Polimorfismo/aula78.js
@@ -9,11 +9,12 @@ function Conta(agencia, conta, saldo) {
 }
 
 Conta.prototype.sacar = function (valor) {
-  if (this.saldo < valor) {
-    this.verSaldo
+  if (valor > this.saldo) {
+    console.log(`Saldo insuficiente: ${this.saldo}`)
     return
   }
   this.saldo -= valor
+  this.verSaldo()
 }
 Conta.prototype.depositar = function (valor) {
   this.saldo += valor
@@ -22,6 +23,12 @@ Conta.prototype.depositar = function (valor) {
 Conta.prototype.verSaldo = function () {
   console.log(
     `Ag/c: ${this.agencia}/${this.conta} | ` +
-      `Saldo: R$${this.saldo.toFixed(2)}`
+      `Saldo: R$ ${this.saldo.toFixed(2)}`
   )
 }
+
+const conta1 = new Conta(11, 22, 10)
+conta1.depositar(11)
+conta1.depositar(10)
+conta1.sacar(30)
+conta1.sacar(0.01)
