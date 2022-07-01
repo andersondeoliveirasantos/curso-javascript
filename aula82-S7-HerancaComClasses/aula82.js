78c91e32744f044c21ca029f18e712dc1a729696
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-30 23:26:39 -0300

    HerancaComClasses.003

--- aula82-S7-HerancaComClasses/aula82.js
+++ aula82-S7-HerancaComClasses/aula82.js
@@ -3,6 +3,15 @@ class DispositivoEletronico() {
     this.nome = nome
     this.ligado = false
   }
+
+ligar() {
+  if(this.ligado) {
+    console.log(this.nome + ' já ligado')
+    return
+  }
 }
 
-ligar
+desligado() {
+
+  }
+}
