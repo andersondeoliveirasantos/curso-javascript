1d1e5ec069005aca54417ccf5cb1d30d081626ad
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-06-30 23:14:03 -0300

    GettersSetters.004

--- aula81-S7-GettersSetters/aula81.js
+++ aula81-S7-GettersSetters/aula81.js
@@ -1,17 +1,28 @@
+const _velocidade = Symbol('velocidade')
 class Carro {
   constructor(nome) {
     this.nome = nome
-    this.velocidade = 0
+    this[_velocidade] = 0
+  }
+
+  set velocidade(valor) {
+    if(typeof valor !== 'number') return
+    if(valor >= 100 || valor <= 0) return
+    this.[_velocidade] = valor
+  }
+
+  get velocidade() {
+    return this[_velocidade]
   }
 
   acelerar() {
-    if (this.velocidade >= 100) return
-    this.velocidade++
+    if (this[_velocidade] >= 100) return
+    this[_velocidade]++
   }
 
   freiar() {
-    if (this.velocidade <= 0) return
-    this.velocidade--
+    if (this[_velocidade] <= 0) return
+    this[_velocidade]--
   }
 }
 
