640707ddc43c5f0c8813356205a9e13197eb57df
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-07-09 23:05:46 -0300

    UsandoClassesValidandoCPF.002

--- aula84-S7-UsandoClassesValidandoCPF/aula84.js
+++ aula84-S7-UsandoClassesValidandoCPF/aula84.js
@@ -1,6 +1,13 @@
 // 705.484.450-52 070.987.720-03
 class ValidaCPF {
-  constructor(cpfEnviado) {}
+  constructor(cpfEnviado) {
+    Object.defineProperty(thi, 'cpsLimpo', {
+      writable: false,
+      enumerable: false,
+      configurable: false
+      value:cpfEnviado.replace(/\D+/g, '')
+    })
+  }
 }
 
 const validacpf = new ValidaCPF('070.987.720-03')
