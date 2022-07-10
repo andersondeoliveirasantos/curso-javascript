dd81e100b7274defe68fb2419ca4e8e9ebd6bb2e
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-07-09 23:15:05 -0300

    UsandoClassesValidandoCPF.004

--- aula84-S7-UsandoClassesValidandoCPF/aula84.js
+++ aula84-S7-UsandoClassesValidandoCPF/aula84.js
@@ -8,10 +8,16 @@ class ValidaCPF {
       value: cpfEnviado.replace(/\D+/g, '')
     })
   }
+
+  éSequência() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+  }
+
   valida() {
     if (!this.cpfLimpo) return false
     if (typeof this.cpfLimpo !== 'string') return false
     if (this.cpfLimpo !== 11) return false
+    if (this.éSequência()) return false
     return 'CHEGUEI AQUI'
   }
 }
