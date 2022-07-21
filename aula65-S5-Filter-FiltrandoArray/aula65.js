419f48dfbc56da870b816387ef04a34306de8670
Anderson de Oliveira Santos <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-07-20 23:36:38 -0300

    Aula65.002

--- aula65-S5-Filter-FiltrandoArray/aula65.js
+++ aula65-S5-Filter-FiltrandoArray/aula65.js
@@ -1,5 +1,7 @@
 // Filter -> Sempre retorna um array, com a mesma quantidade de elementos ou menos
+
 // Retorne os números maiores que 10
+
 // const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 /*
