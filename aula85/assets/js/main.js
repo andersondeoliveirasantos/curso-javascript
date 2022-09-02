e73da6a0f574bceddcd9a004f0af0574d3bedec1
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-01 22:25:15 -0300

    aula85-demo.044

--- aula85/assets/js/main.js
+++ aula85/assets/js/main.js
@@ -3,19 +3,15 @@ class ValidaFormulario {
     this.formulario - document.querySelector('.formalario')
     this.eventos()
   }
-
   eventos() {
     this.formulario.addEventListener('submit', e => {
       this.handleSubmit(e)
     })
   }
-
   handleSubmit(e) {
     e.preventDefault()
     const camposValidos = this.campoSAoValidos()
   }
-
   campoSAoValidos() {}
 }
-
 const valida = new ValidaFormulario()
