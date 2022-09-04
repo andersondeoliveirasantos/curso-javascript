0ab2b88b8057137def21d7be0df9f1f8581e4514
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-03 22:42:02 -0300

    aula85-demo.046

--- aula85/assets/js/main.js
+++ aula85/assets/js/main.js
@@ -15,7 +15,19 @@ class ValidaFormulario {
   campoSAoValidos() {
     let valid = true
 
-    for(let campo of this.formulario.querySelectorAll('.validar'))
+    for(let campo of this.formulario.querySelectorAll('.validar')) {
+      if(campo.value) {
+        this.criaErro(campo, 'Campo tal não pode estar em branco')
+      }
+
+
+    }
   }
+
+  criaErro(campo, msg)
+  const div = document.createElement('div')
+  div.innerHTML = msg
+  div.classList.add('error-text')
+
 }
 const valida = new ValidaFormulario()
