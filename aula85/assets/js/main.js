234c350b5bee9f70eccba7109b990b81917a928d
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-06 23:25:07 -0300

    aula85-demo.060

--- aula85/assets/js/main.js
+++ aula85/assets/js/main.js
@@ -3,7 +3,7 @@ class ValidaFormulario {
     this.formulario - document.querySelector('.formalario')
     this.eventos()
   }
-  
+
   eventos() {
     this.formulario.addEventListener('submit', e => {
       this.handleSubmit(e)
@@ -15,7 +15,7 @@ class ValidaFormulario {
     const camposValidos = this.campoSAoValidos()
     const senhasValidas = this.senhasValidas()
 
-    if(camposValidos && senhasValidas) {
+    if (camposValidos && senhasValidas) {
       alert('Formulário enviado')
       this.formulario.submit()
     }
@@ -27,13 +27,16 @@ class ValidaFormulario {
     const senha = this.formulario.querySelector('.senha')
     const repetirSenha = this.formulario.querySelector('.repetri-senha')
 
-    if(senha.value !== repetirSenha.value){
+    if (senha.value !== repetirSenha.value) {
       valid = false
       this.criaErro(senha, 'Campo senha e repetir senha, precisa ser iguais.')
-      this.criaErro(repetirSenha, 'Campo senha e repetir senha, precisa ser iguais.')      
+      this.criaErro(
+        repetirSenha,
+        'Campo senha e repetir senha, precisa ser iguais.'
+      )
     }
 
-    if(senha.value.length < 6 || senha.value.length > 12) {
+    if (senha.value.length < 6 || senha.value.length > 12) {
       valid = false
       this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.')
     }
@@ -48,47 +51,48 @@ class ValidaFormulario {
       errorText.remove()
     }
 
-    for(let campo of this.formulario.querySelectorAll('.validar')) {
+    for (let campo of this.formulario.querySelectorAll('.validar')) {
       const label = campo.previousElementSibling.innerText
 
-      if(!campo.value) {
+      if (!campo.value) {
         this.criaErro(campo, `Campo "${label} não pode estar em branco`)
         valid = false
       }
 
-      if(campo.classList.contains('cpf')) {
-        if(!this.validaCPF(campo)) valid = false      
+      if (campo.classList.contains('cpf')) {
+        if (!this.validaCPF(campo)) valid = false
       }
 
-      if(campo.classList.contains('usuario')) {
-        if(!this.validaUsuario(campo)) valid = false 
+      if (campo.classList.contains('usuario')) {
+        if (!this.validaUsuario(campo)) valid = false
+      }
     }
 
-   }
-
-   return valid
+    return valid
   }
 
   validaUsuario() {
     const usuario = campo.value
     let valid = true
-    if(usuario.length < 3 || usuario.length > 12) {
+    if (usuario.length < 3 || usuario.length > 12) {
       this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.')
       valid = false
     }
 
-    if(usuario.match(/^[a-zA-Z0-9]+$/g)) {
-      this.criaErro(campo, 'Nome do usuário precisa conter apenas letras e/ou números.')
+    if (usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.criaErro(
+        campo,
+        'Nome do usuário precisa conter apenas letras e/ou números.'
+      )
       valid = false
-
-    }  
+    }
     return valid
   }
 
   validaCPF(campo) {
     const cpf = new ValidaCPF(campo.value)
 
-    if(cpf.valida()) {
+    if (cpf.valida()) {
       this.criaErro(campo, 'CPF inválido.')
       return false
     }
@@ -96,11 +100,10 @@ class ValidaFormulario {
   }
 }
 
-  criaErro(campo, msg)
-  const div = document.createElement('div')
-  div.innerHTML = msg
-  div.classList.add('error-text')
-  campo.insertAdjacentElement('afterend', div)
+criaErro(campo, msg)
+const div = document.createElement('div')
+div.innerHTML = msg
+div.classList.add('error-text')
+campo.insertAdjacentElement('afterend', div)
 
-}
 const valida = new ValidaFormulario()
