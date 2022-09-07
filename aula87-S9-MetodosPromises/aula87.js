66cd465919086b389a578dd97a393e0e4a48ae3b
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-07 18:16:22 -0300

    aula87-MetodosPromises.001

---
+++ aula87-S9-MetodosPromises/aula87.js
@@ -0,0 +1,16 @@
+function rand(min, max) {
+  min *= 1000
+  max *= 1000
+  return Math.floor(Math.random() * (max - mim) + mim)
+}
+
+function esperaAi(msg, tempo)
+  return new Promise((resolve, reject) => {
+    if( typeof msg !== 'string') reject(false)
+    
+    setTimeout(() => {
+      resolve(msg)
+    }, tempo) 
+})
+
+//
