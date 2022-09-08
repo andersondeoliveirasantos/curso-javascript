52bf9a92bbfcb0a52b57eea2fbd036bad96a0404
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-08 11:12:10 -0300

    aula88-S8AsyncAwait.003

--- aula88-S8-AsyncAwait/aula88.js
+++ aula88-S8-AsyncAwait/aula88.js
@@ -17,6 +17,7 @@ function esperaAi(msg, tempo) {
   })
 }
 
+/*
 esperaAi('Fase 1', rand())
   .then(valor => {
     console.log(valor)
@@ -28,4 +29,29 @@ esperaAi('Fase 1', rand())
   })
   .then(fase => {
     console.log(fase)
+    return fase
   })
+  .then(fase => {
+    console.log('Terminamos na fase', fase)
+  })
+  .catch(e => console.log(e))
+*/
+
+async function executa() {
+  try {
+    const fase1 = await esperaAi('Fase 1', rand())
+    console.log(fase1)
+
+    const fase2 = await esperaAi('Fase 2', rand())
+    console.log(fase2)
+
+    const fase3 = await esperaAi('Fase 3', rand())
+    console.log(fase3)
+
+    console.log('Terminamos na fase', fase3)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+executa()
