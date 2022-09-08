73dafda5d3ff4288f8599b5169679cab0a016506
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-08 14:14:01 -0300

    aula90-S8-FetchAPI-GET.003

--- aula90-S8-FetchAPI-GET/assets/js/main.js
+++ aula90-S8-FetchAPI-GET/assets/js/main.js
@@ -15,28 +15,30 @@
 //};
 
 document.addEventListener('click', e => {
-  const el = e.target;
-  const tag = el.tagName.toLowerCase();
+  const el = e.target
+  const tag = el.tagName.toLowerCase()
 
   if (tag === 'a') {
-    e.preventDefault();
-    carregaPagina(el);
+    e.preventDefault()
+    carregaPagina(el)
   }
-});
+})
 
-function carregaPagina(el) {
-  const href = el.getAttribute('href');
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href')
+    const response = await fetch(href)
 
-    fetch(href)
-    .then(response => {
-      if(response.status !== 200) thorw new Error('ERRO 404!')
-      return response.text(
-    })
-    .then(html => carregaResultado(html))
-    .catch(e => console.log(e))
+    if (response.status !== 200) throw new Error('ERRO 404!')
+
+    const html = await response.text()
+    carregaResultado(html)
+  } catch (e) {
+    console.log()
   }
+}
 
-function {
-  const resultado = document.querySelector('.resultado');
-  resultado.innerHTML = response;
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado')
+  resultado.innerHTML = response
 }
