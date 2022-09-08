60789c71f74931a00496292c7c479e89262878f0
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-08 10:42:49 -0300

    aula87-S9-MetodosPromises.005

--- aula87-S9-MetodosPromises/aula87.js
+++ aula87-S9-MetodosPromises/aula87.js
@@ -1,20 +1,23 @@
 function rand(min, max) {
   min *= 1000
   max *= 1000
-  return Math.floor(Math.random() * (max - mim) + mim)
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if (typeof msg !== 'string') reject(false)
-
     setTimeout(() => {
+      if (typeof msg !== 'string') {
+        reject('CAI NO ERRO')
+        return
+      }
+
       resolve(msg.toUpperCase() + ' - Passei na PROMISE')
     }, tempo)
   })
 }
 
-// Promise.all
+// *** Promise.all ***
 /*
 const promises = [
   'Primeiro valor',
@@ -33,12 +36,13 @@ Promise.all(promises)
   })
 */
 
-// Promise.race - Promise.resolve - Promise.reject
-
+// *** Promise.race ***
+/*
 const promises = [
-  esperaAi('Promise 1', 3000),
-  esperaAi('Promise 2', 500),
-  esperaAi('Promise 3', 1000)
+  esperaAi('Promise 1', rand(1, 5)),
+  esperaAi('Promise 2', rand(1, 5)),
+  esperaAi('Promise 3', rand(1, 5))
+  // esperaAi(1000, 1000)
 ]
 
 Promise.race(promises)
@@ -48,3 +52,41 @@ Promise.race(promises)
   .catch(function (erro) {
     console.log(erro)
   })
+*/
+
+// *** Promise.resolve ***
+/*
+function baixaPagina() {
+  const emCache = true
+
+  if (emCache) {
+    return Promise.resolve('Página em cache')
+  } else {
+    return esperaAi('Baixei a página', 3000)
+  }
+}
+
+baixaPagina()
+  .then(dadosPagina => {
+    console.log(dadosPagina)
+  })
+  .catch(e => console.log(e))
+*/
+
+// *** Promise.reject ///
+
+function baixaPagina() {
+  const emCache = true
+
+  if (emCache) {
+    return Promise.reject('Página em cache')
+  } else {
+    return esperaAi('Baixei a página', 3000)
+  }
+}
+
+baixaPagina()
+  .then(dadosPagina => {
+    console.log(dadosPagina)
+  })
+  .catch(e => console.log(e))
