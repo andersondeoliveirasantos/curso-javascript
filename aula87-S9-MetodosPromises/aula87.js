7d14b2743a1ef4796545894b5e8e36471270b493
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-08 10:17:38 -0300

    aula87-S9-MetodosPromises.003

--- aula87-S9-MetodosPromises/aula87.js
+++ aula87-S9-MetodosPromises/aula87.js
@@ -4,13 +4,30 @@ function rand(min, max) {
   return Math.floor(Math.random() * (max - mim) + mim)
 }
 
-function esperaAi(msg, tempo)
+function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if( typeof msg !== 'string') reject(false)
-    
+    if (typeof msg !== 'string') reject(false)
+
     setTimeout(() => {
-      resolve(msg)
-    }, tempo) 
-})
+      resolve(msg.toUpperCase() + ' - Passei na promise')
+    }, tempo)
+  })
+}
 
 // Promise.all - Promise.race - Promise.resolve - Promise.reject
+
+const promises = [
+  'Primeiro valor',
+  esperaAi('Promise 1', 3000),
+  esperaAi('Promise 2', 500),
+  esperaAi('Promise 3', 1000),
+  'Outro valor'
+]
+
+Promise.all(promises)
+  .then(function (valor) {
+    console.log(valor)
+  })
+  .catch(function (erro) {
+    console.log(erro)
+  })
