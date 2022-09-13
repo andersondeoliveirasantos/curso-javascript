3a1f27e5b4484b11c80e94cf347442d7da76a90f
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-13 00:54:24 -0300

    aula93-S9-Webpack-Boilerplate.003

--- Seção 09 Javascript Tooling e ES6 Modules - Módulos/aula93-S9-Webpack-Boilerplate/webpack.config.js
+++ Seção 09 Javascript Tooling e ES6 Modules - Módulos/aula93-S9-Webpack-Boilerplate/webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path') // CommonJS
 
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'Public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {},
+  devtool: 'source-map'
 }
