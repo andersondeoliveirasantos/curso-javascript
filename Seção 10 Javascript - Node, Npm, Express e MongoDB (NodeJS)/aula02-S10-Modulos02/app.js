b2bc912d700cd9b01f5914a2609aca8ae949d38b
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-14 10:18:53 -0300

    aula01-S10-Modulos01.002

---
+++ Seção 10 Javascript - Node, Npm, Express e MongoDB (NodeJS)/aula02-S10-Modulos02/app.js
@@ -0,0 +1,12 @@
+/*
+const Cachorro = require('./B/C/D/mod')
+
+const cachorrinho = new Cachorro('Dog')
+cachorrinho.latir()
+*/
+
+/*
+const multiplicacao = require('./mod')
+
+console.log(multiplicacao(2, 2))
+*/
