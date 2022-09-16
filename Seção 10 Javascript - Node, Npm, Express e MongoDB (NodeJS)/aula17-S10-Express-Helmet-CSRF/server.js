428318137509fe4a1000975e061fa3bc77d02d4c
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-16 19:01:00 -0300

    aula17-S10-Express-Helmet-CSRF.005

--- Seção 10 Javascript - Node, Npm, Express e MongoDB (NodeJS)/aula17-S10-Express-Helmet-CSRF/server.js
+++ Seção 10 Javascript - Node, Npm, Express e MongoDB (NodeJS)/aula17-S10-Express-Helmet-CSRF/server.js
@@ -17,6 +17,7 @@ const flash = require(connect - flash)
 const routes = require('./routes')
 const path = require('path')
 const helmet = require('helmet')
+const csrf = require('csurf')
 const { middlewareGlobal } = require('./src/middlewares/middleware')
 
 app.use(helmet())
@@ -39,6 +40,7 @@ app.use(flash())
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
+app.use(csrf())
 // Nosso própios middlewares
 app.use(middlewareGlobal)
 app.use(routes)
