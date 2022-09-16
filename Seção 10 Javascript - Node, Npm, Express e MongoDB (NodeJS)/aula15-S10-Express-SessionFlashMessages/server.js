d7f20692392e91915268eb876faf4bb6a1a46369
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-16 09:30:44 -0300

    aula15-S10-Express-SessionFlashMessages.001

--- Seção 10 Javascript - Node, Npm, Express e MongoDB (NodeJS)/aula15-S10-Express-SessionFlashMessages/server.js
+++ Seção 10 Javascript - Node, Npm, Express e MongoDB (NodeJS)/aula15-S10-Express-SessionFlashMessages/server.js
@@ -14,6 +14,10 @@ mongoose
   })
   .catch(e => console.log(e))
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+
 const routes = require('./routes')
 const path = require('path')
 const { middlewareGlobal } = require('./src/middlewares/middleware')
@@ -21,6 +25,19 @@ const { middlewareGlobal } = require('./src/middlewares/middleware')
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(path.resolve(__dirname, 'puplic')))
+
+const sessionOptions = session({
+  secret: 'I am from Brasil and I live in São Paulo()',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+})
+app.use(sessionOptions)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
