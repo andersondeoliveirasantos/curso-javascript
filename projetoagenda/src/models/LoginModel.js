6d87ec8f195d042473e2c281d532654b52988604
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-18 16:21:58 -0300

    projetoagenda.030

--- projetoagenda/src/models/LoginModel.js
+++ projetoagenda/src/models/LoginModel.js
@@ -14,10 +14,27 @@ class Login {
     this.user = null
   }
 
+  register() {
+    this.valida()
+  }
+
   valida() {
+    this.clearUp()
     // Validação
     // O e-mail precisar ser válido
     // A senha precisa ter entre 3 e 50
+  }
+  clearUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = ''
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
   }
 }
 
