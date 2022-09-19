11f5e9bc288c37c50dd1ccf34d6e40bb9592a869
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2022-09-19 11:00:02 -0300

    projetoagenda.034

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -9,7 +9,11 @@ exports.register = async function (req, res) {
   await login.register()
 
   if (login.errors.length > 0) {
-    req.flash(errors, login.errors)
+    req.flash('errors', login.errors)
+    req.session.save(function () {
+      return res.redirect('back')
+    })
+    return
   }
 
   res.send(login.errors)
