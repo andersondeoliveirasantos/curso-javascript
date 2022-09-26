b7eaff33d2f60e57c90b49b615858bdd650560f5
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-09-26 17:02:48 -0300

    PI_REST.013

--- S13-JS-API_REST/src/controllers/HomeController.js
+++ S13-JS-API_REST/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from "../models/Aluno";
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: "Daniela",
+      sobrenome: "Cristina da Silva de Oliveira",
+      email: "danicris@outlook.com",
+      idade: 35,
+      peso: 72,
+      altura: 1.74,
     });
+    res.json(novoAluno);
   }
 }
 
