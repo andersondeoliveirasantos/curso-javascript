6c9dbe513964d046e0ccd3b1ab67e4a83c0377a7
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-09-26 15:30:34 -0300

    PI_REST.010

---
+++ S13-JS-API_REST/src/database/migrations/20220926181749-alunos.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: (queryInterface, Sequelize) {
+
+    return queryInterface.dropTable('users')
+  }
+};
