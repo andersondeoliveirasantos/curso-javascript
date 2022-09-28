32e3370204e336331d0121b13db85eecc658c575
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-09-28 10:10:18 -0300

    PI_REST.029

--- S13-JS-API_REST/src/database/seeds/20220928124533-criar-usuarios.js
+++ S13-JS-API_REST/src/database/seeds/20220928124533-criar-usuarios.js
@@ -1,11 +1,30 @@
+const bcryptjs = require("bcryptjs");
+
 module.exports = {
-  up: (queryInterface) =>
+  up: async (queryInterface) =>
     queryInterface.bulkInsert(
-      "People",
+      "users",
       [
         {
-          name: "John Doe",
-          isBetaMember: false,
+          nome: "John Doe",
+          email: "johndoe@gmail.com",
+          password_hash: await bcryptjs.hash("123456", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: "John Doe 2",
+          email: "johndoe2@gmail.com",
+          password_hash: await bcryptjs.hash("654321", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: "John Doe 3",
+          email: "johndoe3@gmail.com",
+          password_hash: await bcryptjs.hash("147852369", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
