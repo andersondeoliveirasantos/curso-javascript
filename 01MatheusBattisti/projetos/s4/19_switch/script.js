0ae4aa84ee60c78ff5fffe0ba110456906304362
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-10 01:16:14 -0300

    switch.001

---
+++ 01MatheusBattisti/projetos/s4/19_switch/script.js
@@ -0,0 +1,14 @@
+let number = 5
+
+while (number < 50) {
+  console.log(number)
+  number += 10
+}
+
+for ( let i = 0; i < 10; i++) {
+  console.log('The i is ' + i)
+}
+
+for ( let i = 10; i > 0; i--) {
+  console.log('The i is ' + i)
+}
