b00a4ba49cbf6efb8a9832c1b02b0faaa1110333
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-10 11:26:21 -0300

    exercise.002

--- 01MatheusBattisti/projetos/s5/01_exercise_06/script.js
+++ 01MatheusBattisti/projetos/s5/01_exercise_06/script.js
@@ -1,7 +1,22 @@
 let name = 'Anderson'
-let age = 41
+let age1 = 41
 let cnh = true
 
 console.log(typeof name)
-console.log(typeof age)
+console.log(typeof age1)
 console.log(typeof cnh)
+
+/* ====================================== */
+
+const firstName = 'Daniela'
+const lastName = 'Cristina da Silva de Oliveira'
+const age2 = 36
+
+console.log(firstName)
+console.log(typeof firstName)
+
+console.log(lastName)
+console.log(typeof lastName)
+
+console.log(age2)
+console.log(typeof age2)
---
+++ 01MatheusBattisti/projetos/s5/02_exercise_07/script.js
@@ -0,0 +1,12 @@
+let firstName = 'Alice'
+let lastName = 'da Silva de Oliveira'
+let age = 6
+
+if (age >= 18) {
+  console.log('You can enter !!!')
+} else {
+  console.log("You can't enter !!!")
+}
+
+/* ================================== */
+
