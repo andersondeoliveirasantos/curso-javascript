0f3a4d7ee172eed15c1499f86b677dc4a489ff2b
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-11 21:04:27 -0300

    exercise.003

--- 01MatheusBattisti/projetos/s5/02_exercise_07/script.js
+++ 01MatheusBattisti/projetos/s5/02_exercise_07/script.js
@@ -10,3 +10,12 @@ if (age >= 18) {
 
 /* ================================== */
 
+let firstName2 = 'Daniela'
+let lastName2 = 'Crsitina da Silva de Oliveira'
+let age2 = 36
+
+if (age => 18) {
+  console.log("Can you get your driver's license")
+} else {
+  console.log("|You can't get your drive's license")
+}
---
+++ 01MatheusBattisti/projetos/s5/03_exercise_08/script.js
@@ -0,0 +1,21 @@
+let allName = 'Anderson de Oliveira Santos'
+
+if(allName == 'Anderson de Oliveira Santos') {
+  console.log(`Be welcome ${allName}`)
+}
+
+/* ============================== */
+
+let allName2 = 'Daniela Cristina da Silva de Oliveira'
+
+if(allName2 == 'Daniela Cristina da Silva de Oliveira') {
+  console.log(`Hello ${allName2}`)
+}
+
+/* ============================== */
+
+let allName3 = 'Alice Silva de Oliveira'
+
+if(allName3 == 'Alice Silva de Oliveira') {
+  console.log(`Hi ${allName3}`)
+}
