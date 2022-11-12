0c8db8cf7f0570d0e40f2acd260215a5dcc4cbf7
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-11 21:17:07 -0300

    exercise.004

---
+++ 01MatheusBattisti/projetos/s5/04_exercise_09/script.js
@@ -0,0 +1,15 @@
+let potency1 = Math.pow(2, 2)
+  console.log(potency1)
+
+let potency2 = Math.pow(3, 2)
+  console.log(potency2)
+
+let potency3 = Math.pow(18, 2)
+  console.log(potency3)
+
+let potency4 = Math.pow(5, 2)
+  console.log(potency4)
+
+console.log(Math.pow(4, 2))
+console.log(Math.pow(10, 2))
+console.log(Math.pow(20, 2))
