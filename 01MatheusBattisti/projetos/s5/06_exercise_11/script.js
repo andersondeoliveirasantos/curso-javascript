4dc20c3e034588febaf0e5d17a8697229aec7c76
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-12 23:51:20 -0300

    exercise.007

--- 01MatheusBattisti/projetos/s5/06_exercise_11/script.js
+++ 01MatheusBattisti/projetos/s5/06_exercise_11/script.js
@@ -1,7 +1,3 @@
-let velocity = 81
-
-if (velocity <= 80) {
-  console.log(`At the allowed speed`)
-} else {
-  console.log(`You are over the speed limit`)
-}
+let name = 'Anderson'
+let age = 41
+let 
