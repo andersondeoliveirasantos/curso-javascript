957f04b0083926a1eafb15b76c1036ffbd5e19ed
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-13 03:06:27 -0300

    exercise.008

--- 01MatheusBattisti/projetos/s5/06_exercise_11/script.js
+++ 01MatheusBattisti/projetos/s5/06_exercise_11/script.js
@@ -1,3 +1,11 @@
 let name = 'Anderson'
 let age = 41
-let 
+let cnh = true
+
+if (age >=18 && cnh == true) {
+  console.log('Yes, you can drive')
+} else if (age >= 18 && cnh == false) {
+  console.log("No, you can't drive, but you can get your driver's license")
+} else { 
+  console.log("No, you can't drive")
+}
