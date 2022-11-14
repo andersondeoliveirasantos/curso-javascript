0a5bdcb3a22277784e5dd5774611d66c3599b9c3
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-14 13:59:56 -0300

    exercise.009

---
+++ 01MatheusBattisti/projetos/s5/07_exercise_12/script.js
@@ -0,0 +1,21 @@
+let i = 0 
+let x = 2
+let y = 5
+
+while (i <= 10) {
+  console.log(i)
+
+  i++
+}
+
+while (x <= 5) {
+  console.log(x)
+
+  x++
+}
+
+while (y <= 10) {
+  console.log(y)
+
+  y++
+}
