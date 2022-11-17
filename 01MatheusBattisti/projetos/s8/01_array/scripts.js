97e5e6e9f3207bf54afa0e71532e659384626e77
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-17 01:13:57 -0300

    arraysObjetos.001

---
+++ 01MatheusBattisti/projetos/s8/01_array/scripts.js
@@ -0,0 +1,12 @@
+let arr = [1, 4, 5, 6, 7]
+
+let names = ['Anderson', 'Daniela', 'Alice']
+
+let bool = [true, false, true]
+
+let mixed = ['Anderson', true,  41]
+
+console.log(arr)
+console.log(names)
+console.log(bool)
+console.log(mixed)
---
+++ 01MatheusBattisti/projetos/s8/02_more_about_arrays/scripts.js
@@ -0,0 +1,14 @@
+let arr = [1, 4, 5, 6, 7]
+
+let names = ['Anderson', 'Daniela', 'Alice']
+
+let bool = [true, false, true]
+
+let mixed = ['Anderson', true,  41]
+
+console.log(arr[1])
+console.log(names[0])
+console.log(bool[2])
+console.log(mixed[2])
+
+console.log(arr[arr.length - 1])
