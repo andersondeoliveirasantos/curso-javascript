5d8947b76cc4f7dbc774193a03f4924c15a7ff80
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-18 15:01:48 -0300

    arraysObjects.007

---
+++ 01MatheusBattisti/projetos/s8/16_includes/scripts.js
@@ -0,0 +1,4 @@
+let cars = ['BMW', 'Fiat', 'Vw', 'Renalt', 'Audi']
+
+console.log(cars.includes('Fiat')) // Confirms if the element exists or not
+console.log(cars.includes('Ford')) // confirmando que o elemento não existe
---
+++ 01MatheusBattisti/projetos/s8/18_trim/scripts.js
@@ -0,0 +1,9 @@
+let cars = ['BMW', 'Fiat', 'Vw', 'Renalt', 'Audi']
+
+console.log(cars.reverse()) // Reverts the objects
+
+
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(numbers.reverse())
