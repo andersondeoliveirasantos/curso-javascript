dbab9c356918d68bfb4f50cde776dc87577da978
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-17 22:34:11 -0300

    arraysObjetos.003

---
+++ 01MatheusBattisti/projetos/s8/05_objects/scripts.js
@@ -0,0 +1,14 @@
+let dog = {
+  foldes: 4,
+  name: 'Shark',
+  bark: function() {
+    console.log('Au Au ...')
+  }
+}
+
+console.log(dog.foldes)
+console.log(dog.name)
+dog.bark()
+
+// [ ] = arrays
+// { } = objects
---
+++ 01MatheusBattisti/projetos/s8/06_objects_create_prop/scripts.js
@@ -0,0 +1,15 @@
+let person = {
+  name: 'Anderson',
+  age: 41,
+  profession: 'Developer'
+}
+
+console.log(person.name)
+
+delete person.name
+
+console.log(person.name)
+
+person.married = false
+
+console.log(person.married)
