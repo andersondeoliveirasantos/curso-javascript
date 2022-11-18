5c704e624703b5251ed7b4ea74e285dd9cc49058
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-18 00:56:27 -0300

    arraysObjetos.004

---
+++ 01MatheusBattisti/projetos/s8/07_assing/scripts.js
@@ -0,0 +1,16 @@
+let car = {
+  doors: 2,
+  doorsTrunk: '200 litros',
+  motor: '2.0',
+}
+
+let additional = {
+  sunroof: true,
+  airConditioning: true,
+}
+
+console.log(car)
+
+object.assing(car, additional)
+
+console.log(car)
