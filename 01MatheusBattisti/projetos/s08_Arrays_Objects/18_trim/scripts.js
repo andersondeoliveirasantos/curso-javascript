156113f3b4c4fddc7d58283234a42261e34695d2
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-18 15:20:02 -0300

    arraysObjects.008

---
+++ 01MatheusBattisti/projetos/s08_Arrays_Objects/18_trim/scripts.js
@@ -0,0 +1,10 @@
+let name = '      Anderson de Oliveira Santos'
+
+// Hello          Anderson  , how are you?
+
+let correctedName = name.trim()
+
+console.log(name)
+console.log(correctedName)
+
+// Remove linhas e caracteres especiais.
