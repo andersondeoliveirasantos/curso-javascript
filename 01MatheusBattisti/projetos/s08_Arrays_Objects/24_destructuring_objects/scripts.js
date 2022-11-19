da9750b253621edbecabb2aaf66dad6ac162247c
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-18 23:38:15 -0300

    arraysObjects.010

---
+++ 01MatheusBattisti/projetos/s08_Arrays_Objects/24_destructuring_objects/scripts.js
@@ -0,0 +1,11 @@
+let object = {
+  wheels: 4,
+  doors: 4,
+  sunroof: true,
+  engine: 2.0
+}
+
+const {wheels: vWheels, doors: vDoors, sunroof: vSunroof, engine: vEngine} = object 
+
+console.log(vWheels)
+console.log(vDoors)
---
+++ 01MatheusBattisti/projetos/s08_Arrays_Objects/25_destructuring_arrays/scripts.js
@@ -0,0 +1,6 @@
+let numbers = [2, 4, 5, 8]
+
+let [number1, number2, number3, number4] = numbers
+
+console.log(number1)
+console.log(number3)
