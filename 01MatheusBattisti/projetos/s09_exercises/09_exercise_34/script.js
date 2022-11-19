7c6bafbe5c98a8bd93bdd152ea45b8c50e24d969
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-19 08:37:45 -0300

    exercisesArrayObjects.003

---
+++ 01MatheusBattisti/projetos/s09_exercises/09_exercise_34/script.js
@@ -0,0 +1,7 @@
+const phrase = 'Being a good programmer takes a lot of prectice.'
+
+const arrPhrase = phrase.split(' ')
+
+for(let i = 0; i < arrPhrase.length; i++) {
+  console.log(arrPhrase[i])
+}
