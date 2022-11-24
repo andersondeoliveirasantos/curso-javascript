82229f418f8a1e5f87c2e2e25de5c89840723f52
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-24 11:27:06 -0300

    s13.regularExpressions.002

--- 01MatheusBattisti/projetos/s13/03_specialCharacters/script.js
+++ 01MatheusBattisti/projetos/s13/03_specialCharacters/script.js
@@ -1,44 +1,17 @@
-const scoreDot = /./ // Ponto regex
+const day = /\d\d/
 
-console.log('.')
-console.log(scoreDot.test('ads'))
-console.log(scoreDot.test('  '))
-console.log(scoreDot.test('123'))
-console.log(scoreDot.test('123sad'))
+console.log(day.test('2019'))
+console.log(day.test('2019') && '2019'.length == 2) // More specific
 
-console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+console.log(day.test('asd'))
 
-const dRegex = /\d/ // O mesmo que => [0-9]
+console.log(day.test('05'))
+console.log(day.test('05') && '05'.length == 2) // More specific
 
-console.log('d')
-console.log(dRegex.test('ads'))
-console.log(dRegex.test('  '))
-console.log(dRegex.test('123'))
-console.log(dRegex.test('123sad'))
+console.log(day.test('asd12'))
 
-console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+const threeLetterWords = /\w\w\w/
 
-const dRegex2 = /\D/ 
-console.log('D')
-console.log(dRegex2.test('ads'))
-console.log(dRegex2.test('  '))
-console.log(dRegex2.test('123'))
-console.log(dRegex2.test('123sad'))
-
-console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
-
-const sRegex = /\s/ 
-console.log('s')
-console.log(sRegex.test('ads'))
-console.log(sRegex.test('  '))
-console.log(sRegex.test('123'))
-console.log(sRegex.test('123sad'))
-
-console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
-
-const wRegex = /\w/ 
-console.log('w')
-console.log(wRegex.test('ads'))
-console.log(wRegex.test('  '))
-console.log(wRegex.test('123'))
-console.log(wRegex.test('123sad'))
+console.log(threeLetterWords.test('asd'))
+console.log(threeLetterWords.test('asdd'))
+console.log(threeLetterWords.test('as'))
