c656ca92ae66fe583c83690e1bdebc618b55aaa9
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-27 19:09:10 -0300

    s16_js_browser.002

---
+++ 01MatheusBattisti/projetos/s16/03_move_in_the_dom/js/script.js
@@ -0,0 +1,9 @@
+console.log(document.body)
+
+console.log(document.body.childNodes[1])
+
+console.log(document.body.childNodes[1].ChildNodes)
+
+console.log(document.body.childNodes[1].ChildNodes[1])
+
+console.log(document.body.childNodes[1].ChildNodes[1].innerText)
