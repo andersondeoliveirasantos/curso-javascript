4d6ce439bb4abaed55fc066aa87012a201ebf20d
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-29 10:20:37 -0300

    s16_js_browser.008

---
+++ 01MatheusBattisti/projetos/s16/16_changing_styles/js/script.js
@@ -0,0 +1,7 @@
+let element = document.querySelector('#main-title')
+
+setTimeout(function() {
+  element.style.color = 'red'
+  element.style.backgroundColor = 'yellow'
+  element.style.width = '400px'
+}, 1000)
---
+++ 01MatheusBattisti/projetos/s16/17_select_several/js/script.js
@@ -0,0 +1,7 @@
+let items = document.querySelectorAll('.red-items')
+
+console.log(items)
+
+let items2 = document.querySelectorAll('.blue-items')
+
+console.log(items2)
