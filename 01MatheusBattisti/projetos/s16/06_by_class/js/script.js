a6019ca6031f9dd9691913e2a073ca731448224a
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-28 23:01:45 -0300

    s16_js_browser.004

---
+++ 01MatheusBattisti/projetos/s16/06_by_class/js/script.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName('li'))
+
+console.log(document.getElementsByClassName('blue-items'))
+
+console.log(document.getElementsByClassName('red-items'))
+
+console.log(document.getElementsByClassName('items'))
---
+++ 01MatheusBattisti/projetos/s16/07_by_query/js/script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('#main-container h1'))
+
+console.log(document.querySelector('#second-container h1'))
+
+console.log(document.querySelector('div div p'))
+
+console.log(document.querySelector('footer h2'))
+
+console.log(document.querySelector('ul .blue-items'))
