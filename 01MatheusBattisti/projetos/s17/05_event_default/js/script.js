28d8ca6db73777070ba86db73d7211a9b3bc2ea4
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-30 16:27:15 -0300

    s17_events_javascript.003

---
+++ 01MatheusBattisti/projetos/s17/05_event_default/js/script.js
@@ -0,0 +1,24 @@
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let p = document.querySelector('p')
+let a = document.querySelector('a')
+
+function msg(e) {
+  console.log('Clicked the button');
+  e.stopPropagation()
+}
+
+btn1.addEventListener('click', msg) 
+
+btn2.addEventListener('click', function(event) {
+  console.log(event)
+})
+
+p.addEventListener('click', function() {
+  console.log('Cliched on the paragraph')
+})
+
+a.addEventListener('click', function(e) {
+  e.preventDefault()
+  console.log('Will not change link') // Não vai mudar de link
+})
---
+++ 01MatheusBattisti/projetos/s17/06_key_event/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('keydown', function(e) {
+  if(e.key == 'q') {
+    console.log('Pressed the letter "q"')
+  }
+})
+
+window.addEventListener('keyup', function(e) {
+  if(e.key = 'Enter') {
+    console.log('Released the enter')
+  }
+ })
