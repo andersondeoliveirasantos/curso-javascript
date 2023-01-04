4fd5095a737665e2d3f8352aed8daa4338e973cb
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2023-01-03 22:54:21 -0300

    s19_Node.js.007

--- 01MatheusBattisti/projetos/s19/05_express/index.js
+++ 01MatheusBattisti/projetos/s19/05_express/index.js
@@ -1,10 +1,12 @@
 const {createServer} = require('http');
   let server = createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
+    
     response.write(`
       <h1>Hello Word !!!</h1>
       <p>Primeira página com Node.js</p>
       `);
+
       response.end();
   })
 
